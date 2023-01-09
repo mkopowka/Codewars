75c76c1853b7471beb28f48de8e04386450b6b09
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-09 17:42:33 +0100

    CheckForFactor

---
+++ Codewars/CheckFactor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class CheckFactor
+    {
+        public static bool CheckForFactor(int num, int factor)
+        {
+            if (num % factor == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Codewars;
+
 Console.WriteLine("Hello, World!");
