9ae6f477c65c96a40b54859895e6da2a835a3822
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-10 17:27:46 +0100

    BreakCamelCase

---
+++ Codewars/BreakCamelCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class BreakCamelCase
+    {
+        public static string BreakCamelCases(string str)
+        {
+            string words = "";
+            foreach (char ch in str)
+            {
+                if (char.IsLower(ch))
+                {
+                    words += ch.ToString();
+                }
+                else
+                {
+                    words += " " + ch.ToString();
+                }
+
+            }
+            return words;
+        }
+    }
+}
