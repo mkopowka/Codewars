aa29fe3148f38d4bc5fdd343a1f5795206557bf2
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-10 16:54:57 +0100

    OtherAngle

---
+++ Codewars/OtherAngle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class OtherAngle
+    {
+        public int OtherAngle(int a, int b)
+        {
+            int c = 180 - (a + b);
+
+            return c;
+        }
+    }
+}
