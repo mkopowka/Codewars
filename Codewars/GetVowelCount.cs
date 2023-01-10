0a517ec5a312723bd2dd1dc3635436a981220816
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-10 21:36:37 +0100

    GetVowelCount

---
+++ Codewars/GetVowelCount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetVowelCount
+    {
+        public static int GetVowelCountz(string str)
+        {
+            int vowelCount = 0;
+
+            char[] chars = str.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == 'a' || chars[i] == 'e' || chars[i] == 'i' || chars[i] == 'o' || chars[i] == 'u')
+                {
+                    vowelCount++;
+                }
+            }
+            return vowelCount;
+        }
+    }
+}
