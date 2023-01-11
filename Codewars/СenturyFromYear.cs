ba80fbe4a9a407c6975afff437d1ed4fb2a5fa3c
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-11 19:38:24 +0100

    СenturyFromYearz

---
+++ Codewars/СenturyFromYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class СenturyFromYearz
+    {
+        public static int СenturyFromYear(int year)
+        {
+            int century = year / 100;
+            year %= 100;
+            if (year > 0)
+            {
+                century += 1;
+            }
+            return century;
+        }
+    }
+}
