ea593cf8a050a604167dbcfee4e0a1ff6f608ff9
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-11 20:15:04 +0100

    FriendOrFoe

---
+++ Codewars/FriendOrFoe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FriendOrFoe
+    {
+        public IEnumerable<string> FriendOrFoes(string[] names)
+        {
+
+            List<string> friends = names.Where(name => name.Length==4).ToList();
+
+            return friends;
+        }
+    }
+}
