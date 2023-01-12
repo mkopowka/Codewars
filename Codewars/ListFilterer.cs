5538445f90d06971b96dc97dda16cfd028cedc84
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-12 21:18:46 +0100

    ListFilterer

---
+++ Codewars/ListFilterer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ListFilterer
+    {
+        public IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+
+        {
+            return  listOfItems.OfType<int>().ToList();
+        }
+    }
+}
