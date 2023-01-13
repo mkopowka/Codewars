60438229e02d6c381310aad39c59ee729225c9be
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-13 21:48:21 +0100

    ValidatePin

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -7,6 +7,8 @@ CheckIsTriange checkIsTriange= new CheckIsTriange();
 CheckIsSquare checkIsSquare = new CheckIsSquare();
 MinMax minMax = new MinMax();
 СenturyFromYear сenturyFromYear = new СenturyFromYear();
+ValidatePin validatePin = new ValidatePin();
+validatePin.ValidatePinz("-1234");
 checkIsTriange.IsTriangle(1, 2, 4);
 checkIsSquare.IsSquare(5);
 minMax.Min(new[] { -32, 42, 2, 6, 7, 95 });
---
+++ Codewars/ValidatePin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ValidatePin
+    {
+        public bool ValidatePinz(string pin)
+        {
+            int parse = 0;
+            if (pin.Length == 4 || pin.Length == 6)
+            return int.TryParse(pin,out parse);
+                else
+            return false;
+        }
+    }
+}
