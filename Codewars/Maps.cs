90fefd22794303dc88cd54667169fe8099de04a8
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-15 20:12:26 +0100

    Maps

---
+++ Codewars/Maps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Maps
+    {
+        public static int[] Map(int[] x)
+        {
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] *= 2;
+            }
+
+            return x;
+        }
+    }
+}
