2fd38610259934a3ef375bf3c0e42d7e25d314e6
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-15 19:43:15 +0100

    IsPangram

---
+++ Codewars/IsPanagram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class IsPanagram
+    {
+        public static bool IsPangram(string str)
+        {
+            return str.ToLower().Where(ch => Char.IsLetter(ch)).GroupBy(ch => ch).Count() == 26;
+        }
+    }
+}
