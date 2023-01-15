5d089c759ac92341198670108cb2b68a3097ea5a
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-15 20:17:51 +0100

    EvenOrOdd

---
+++ Codewars/Even0rOdd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Even0rOdd
+    {
+        public string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+                return "Odd";
+        }
+    }
+}
