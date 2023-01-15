6a0102732e40850dac44ff4d82adec4b19f4a713
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-15 19:46:03 +0100

    HoopCount

---
+++ Codewars/HoopCount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class HoopCount
+    {
+        public static string HoopCounts(int n)
+        {
+            string s = " ";
+
+            if (n < 10)
+            {
+                s = "Keep at it until you get it";
+            }
+            else if (n > 10)
+            {
+                s = "Great, now move on to tricks";
+            }
+            return s;
+        }
+    }
+}
