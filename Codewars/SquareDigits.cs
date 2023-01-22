f56f3f857c644af56a855bed60cdce615fbd3843
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-22 20:08:05 +0100

    SquareDigitsc

---
+++ Codewars/SquareDigits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SquareDigits
+    {
+        public int SquareDigit(int n)
+        {
+            string input = n.ToString();
+            string result = "";
+            foreach (char c in input)
+            {
+                int digit = int.Parse(c.ToString());
+                result += (digit * digit);
+            }
+            return int.Parse(result);
+        }
+    }
+}
