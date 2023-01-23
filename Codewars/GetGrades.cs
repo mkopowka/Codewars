22eb798f8c2c4c27d8a0c9b03de73b66ff59973e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-23 22:25:21 +0100

    GetGrade

---
+++ Codewars/GetGrades.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetGrades
+    {
+        public static char GetGrade(int s1, int s2, int s3)
+        {
+            int sum = s1 + s2 + s3;
+            int score =sum / 3;
+            if (score >= 90)
+            {
+                return 'A';
+            }
+            else if (score < 90 && score >= 80)
+            {
+                return 'B';
+            }
+            else if (score < 80 && score >= 70)
+            {
+                return 'C';
+            }
+            else if (score < 70 && score >= 60)
+            {
+                return 'D';
+            }
+            else
+                return 'F';
+
+        }
+    }
+}
