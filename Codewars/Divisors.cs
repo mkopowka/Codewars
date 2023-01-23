b55dfa449f5e7fac7ddf0341a38c36d44b04f078
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-23 22:07:01 +0100

    Divisors

---
+++ Codewars/Divisors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Divisors
+    {
+        public static int Divisor(int n)
+        {
+            int cnt = 0;
+            for (int i = 1; i <= Math.Sqrt(n);i++)
+            {
+                if (n % i == 0)
+                {
+
+                    if (n / i == i)
+                        cnt++;
+
+                    else
+                        cnt = cnt + 2;
+                }
+            }
+
+            return cnt;
+        }
+    }
+}
