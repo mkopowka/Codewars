b17f6db6874ed60ff1a380521411181ca66c4b23
Mateusz Kopówka <mkopowka@hotmail.com>
2023-01-23 22:13:21 +0100

    SwitchItUp

---
+++ Codewars/SwitchItUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SwitchItUp
+    {
+        public static string SwitchItUps(int number)
+        {
+            if (number == 1)
+                return "One";
+            else if (number == 2)
+                return "Two";
+            else if (number == 3)
+                return "Three";
+            else if (number == 4)
+                return "Four";
+            else if (number == 5)
+                return "Five";
+            else if (number == 6)
+                return "Six";
+            else if (number == 7)
+                return "Seven";
+            else if (number == 8)
+                return "Eight";
+            else if (number == 9)
+                return "Nine";
+            else
+            return "Zero";
+        }
+    }
+}
