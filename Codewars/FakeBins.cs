f0f5fe3df34160b93a6d853004568e92f933b94e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-05 23:11:46 +0100

    FakeBins

---
+++ Codewars/FakeBins.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FakeBins
+    {
+        public static string FakeBin(string x)
+        {
+            string result = "";
+            foreach (char c in x)
+            {
+                result += int.Parse(c.ToString()) < 5 ? "0" : "1";
+            }
+            return result;
+        }
+    }
+}
