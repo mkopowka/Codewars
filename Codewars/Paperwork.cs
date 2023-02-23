776a812b8d181ed79779c6d5a1ebbff42ff822a0
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-23 12:18:42 +0100

    Paperworks

---
+++ Codewars/Paperwork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Paperwork
+    {
+        public int Paperworks(int n, int m)
+        {
+            if (n > 0 && m > 0)
+            {
+                return n * m;
+            }
+            else
+                return 0;
+        }
+    }
+}
