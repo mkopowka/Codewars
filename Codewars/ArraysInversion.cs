8e9ed40c677304000341e41268554791d27e8798
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-24 10:08:09 +0100

    ArraysInversion

---
+++ Codewars/ArraysInversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ArraysInversion
+    {
+        public int[] InvertValues(int[] input)
+        {
+
+            int[] result = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                result[i]= input[i]*-1;
+            }
+            return result;
+        }
+    }
+}
