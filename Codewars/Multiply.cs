94ff063800dc3b1fc3adf339399085854a0b1001
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-24 10:39:27 +0100

    Multiply

---
+++ Codewars/Multiply.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Multiply
+    {
+
+        public static int Multiplies(int x)
+        {
+            if (x% 2 == 0) return x * 8; else return x * 9;  
+
+        }
+    }
+}
