69876bcbae8ccacf8090c2ad4a99909612c1a646
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-24 10:42:59 +0100

    AreaOrPerimeter

---
+++ Codewars/AreaOrPerimeter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class AreaOrPerimeter
+    {
+        public static int AreaOrPerimeters(int l, int w)
+        {
+            if (l == w)
+            {
+                return l * w;
+            }
+            else
+                return 2 * l + 2 * w;
+        }
+    }
+}
