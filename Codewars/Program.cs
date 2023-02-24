9d0e1d45d007993772f701a122bbe3791f933160
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-24 10:31:01 +0100

    Summation

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -9,9 +9,12 @@ MinMax minMax = new MinMax();
 СenturyFromYear сenturyFromYear = new СenturyFromYear();
 ValidatePin validatePin = new ValidatePin();
 Xoxo xoxo = new Xoxo();
+Summation sum= new Summation();
 xoxo.XO("xo");
 validatePin.ValidatePinz("-1234");
 checkIsTriange.IsTriangle(1, 2, 4);
 checkIsSquare.IsSquare(5);
 minMax.Min(new[] { -32, 42, 2, 6, 7, 95 });
 сenturyFromYear.СenturyFromYears(1902);
+sum.summation(5);
+
---
+++ Codewars/Summation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Summation
+    {
+        public int summation(int num)
+        {
+            int result = 0;
+            for (int i=1;i<num+1;i++)
+            {
+                result+= i;
+            }
+
+            return result;
+    }
+    }
+}
