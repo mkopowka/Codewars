60b8645da9514c722180143e4954f83aeef7a62f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-25 12:24:44 +0100

    PositiveSums

---
+++ Codewars/PositiveSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class PositiveSum
+    {
+        public static int PositiveSums(int[] arr)
+        {
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    result += arr[i];
+                }
+            }
+            return result;
+        }
+    }
+}
