5108068164c472643d5bca17e1954b6dba9cc63c
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-25 23:08:29 +0100

    SumArrays

---
+++ Codewars/SumArrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumArrays
+    {
+        public static double SumArray(double[] array)
+        {
+            double sum = 0;
+            if(array==null) return 0;
+            else
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            return sum;
+         }
+    }
+}
