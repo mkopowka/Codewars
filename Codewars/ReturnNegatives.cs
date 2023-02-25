f6ba32612ec1b89853a00f72a3dd72e298cbe4f3
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-25 23:23:15 +0100

    ReturnNegatives

---
+++ Codewars/ReturnNegatives.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ReturnNegatives
+    {
+        public static int MakeNegative(int number)
+        {
+            // Code?
+            return number*-1;
+        }
+    }
+}
