de2b827d17b9f280753aecd45d89ee06979adde1
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-26 22:16:07 +0100

    FinalGrades fix

--- Codewars/FinalGrade.cs
+++ Codewars/FinalGrade.cs
@@ -10,21 +10,29 @@ namespace Codewars
     {
         public static int FinalGrades(int exam, int projects)
         {
-            int final = 0;
-            if (exam > 90 || projects >= 10)
+            int final;
+            if (projects > 10)
             {
-                final = 100;
+                 final = 100;
+            }
+            else if (exam > 90)
+            {
+                 final = 100;
+            }
+            else if (exam > 90 && projects >= 10)
+            {
+                 final = 100;
             }
             else if (exam > 75 && projects >= 5)
             {
-                final = 90;
+                 final = 90;
             }
             else if (exam > 50 && projects >= 2)
             {
-                final = 75;
+                 final = 75;
             }
             else
-                final = 0;
+                 final = 0;
             return final;
         }
     }
