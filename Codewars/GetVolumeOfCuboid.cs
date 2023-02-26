4108ab5c8232b8a0d248d1d6701889e0f373db06
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-26 22:01:54 +0100

    GetVolumeOfCuboid

---
+++ Codewars/GetVolumeOfCuboid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetVolumeOfCuboid
+    {
+        public static double GetVolumeOfCuboids(double length, double width, double height)
+        {
+
+            return length * width * height;
+        }
+    }
+}
