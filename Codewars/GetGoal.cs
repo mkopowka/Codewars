7ce8c0bc3e852572f8b72958ccf1659b19b5ea52
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-26 10:57:52 +0100

    GetGoals

---
+++ Codewars/GetGoal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetGoal
+    {
+         public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+        {
+            return laLigaGoals+ copaDelReyGoals + championsLeagueGoals;
+        }
+    }
+}
