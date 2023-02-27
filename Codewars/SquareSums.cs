abd4a611b4e302a59be7d8f5d488205013884e6d
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-27 17:17:43 +0100

    SquareSums

---
+++ Codewars/SquareSums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SquareSums
+    {
+        public static int SquareSum(int[] numbers)
+        {
+            int sum = 0;
+            int b = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                b=numbers[i] * numbers[i];
+                sum += b;
+            }
+
+            return sum;
+        }
+    }
+}
