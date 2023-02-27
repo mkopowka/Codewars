4574fbcf26428421868ca0c04c6b4bb2d55ef766
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-27 17:15:28 +0100

    BasicOperations

---
+++ Codewars/basicOp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class basicOp
+    {
+        public static double basicOps(char operation, double value1, double value2)
+        {
+            if (operation == '+') return value1 + value2;
+            if (operation=='-') return value1 - value2;
+            if (operation=='*') return value1 * value2;
+            if (operation == '/') return value1 / value2;
+            else return -1;
+        }
+    }
+}
