4aff3f0ba6ae40512e17bdb3a39fddfbeeb0f6f9
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-27 12:53:53 +0100

    Remove_chars

---
+++ Codewars/Remove_chars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Remove_chars
+    {
+        public static string Remove_char(string s)
+        {
+            s.ToLower();
+            s = s.Remove(s.Length - 1);
+            string result = s.Remove(0, 1);
+            return result;
+        }
+    }
+}
