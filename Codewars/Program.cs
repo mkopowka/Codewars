2578cdba3181f2552b43b1754368603c0f546847
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-28 22:05:07 +0100

    RentalCarCost

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -11,6 +11,7 @@ ValidatePin validatePin = new ValidatePin();
 Xoxo xoxo = new Xoxo();
 Summation sum= new Summation();
 SetAlarm alarm= new SetAlarm();
+RentalCarCosts rs= new RentalCarCosts();
 xoxo.XO("xo");
 validatePin.ValidatePinz("-1234");
 checkIsTriange.IsTriangle(1, 2, 4);
@@ -19,4 +20,5 @@ minMax.Min(new[] { -32, 42, 2, 6, 7, 95 });
 сenturyFromYear.СenturyFromYears(1902);
 sum.summation(5);
 alarm.SetAlarms(true, true);
+rs.RentalCarCost(7);
 
---
+++ Codewars/RentalCarCosts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class RentalCarCosts
+    {
+        public int RentalCarCost(int d)
+        {
+            int result = 0;
+            if (d < 3)
+            {
+                result = 40 * d;
+            }
+            else if (d >= 3 && d < 7)
+            {
+                result = 40 * d - 20;
+            }
+            if (d >= 7)
+            {
+                result = 40 * d - 50;
+            }
+            return result;
+        }
+    }
+}
