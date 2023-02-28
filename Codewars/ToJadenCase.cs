d907e359ddd9090812553222a7064637c577ee44
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-28 17:52:46 +0100

    ToJadenCase

---
+++ Codewars/ToJadenCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ToJadenCase
+    {
+        public static string ToJadenCase(this string phrase)
+        {
+            string[] words = phrase.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+            }
+
+            string capitalizedString = string.Join(" ", words);
+            return capitalizedString;
+        }
+    }
+}
