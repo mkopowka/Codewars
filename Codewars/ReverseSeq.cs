c51ecb64741bf9414009a2c80cea4e6b5b0bf6a2
Mateusz Kopówka <mkopowka@hotmail.com>
2023-02-28 17:45:12 +0100

    ReverseSeq

---
+++ Codewars/ReverseSeq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ReverseSeq
+    {
+        public static int[] ReverseSeqs(int n)
+        {
+            int[] result = new int[n];
+            for(int i = 0; i < n;i++)
+            {
+                result[i] = n-i;
+            }
+            return result;
+        }
+    }
+}
