3e8e4a37bf6c827e5fbff4d41bc1f4388abe2279
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-03 08:55:17 +0100

    AreYouPlayingBanjo

---
+++ Codewars/AreYouPlayingBanjo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class AreYouPlayingBanjo
+    {
+        public static string AreYouPlayingBanjos(string name)
+        {
+            if (name.StartsWith('r') || name.StartsWith('R'))
+            {
+                return name + " plays banjo";
+            }
+            else
+                return name + " does not play banjo";
+        }
+    }
+}
