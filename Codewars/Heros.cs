a2e2f3b0bc5df99b85220e79f974ecd6a1c59540
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-04 11:10:01 +0100

    Heros

---
+++ Codewars/Heros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Heros
+    {
+        public static bool Hero(int bullets, int dragons)
+        {
+            if (bullets / dragons >= 2)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
