248fbd8f72ab2e2df28d5359bf1d33120dbdac94
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-05 19:28:35 +0100

    ReverseArray

---
+++ Codewars/ReverseArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ReverseArray
+    {
+        public static string Solution(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
