9f4116e9b8595413c15fa319637840257e8346f4
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-05 19:40:21 +0100

    LoverDetectors

---
+++ Codewars/LoverDetectors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class LoverDetectors
+    {
+        public static bool lovefunc(int flower1, int flower2)
+        {
+            if(flower1%2 == 0 && flower2%2==0) { return false; }
+            else { return true; }
+        }
+    }
+}
