92774e08331b421f32a3e541d9398e0f6ecee39d
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-06 20:16:05 +0100

    Greets

---
+++ Codewars/Greets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Greets
+    {
+        public static string Greet(string name, string owner)
+        {
+
+            if (name == owner)
+            {
+                return "Hello boss";
+            }
+            else
+
+                return "Hello guest";
+        }
+    }
+}
