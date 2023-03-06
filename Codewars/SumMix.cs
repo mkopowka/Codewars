5bdad20840e17c31464f1c27e47a0a4ce9a4822f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-06 20:24:46 +0100

    SumMix

---
+++ Codewars/SumMix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumMix
+    {
+        public static int SumMixs(object[] x)
+        {
+            int total = 0;
+            foreach (object item in x)
+            {
+                if (item is int)
+                {
+                    total += (int)item;
+                }
+                else if (item is string && int.TryParse((string)item, out int intValue))
+                {
+                    total += intValue;
+                }
+            }
+            return total;
+        }
+    }
+}
