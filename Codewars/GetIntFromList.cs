114ead0caded013a00850cd2473ded06854727d9
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-07 20:46:45 +0100

    GetIntegersFromList

---
+++ Codewars/GetIntFromList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetIntFromList
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            return listOfItems.OfType<int>().ToList();
+        }
+    }
+}
