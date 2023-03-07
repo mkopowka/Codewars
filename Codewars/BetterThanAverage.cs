d18a8b9d07c4835c6bbae867acb9531fa2ad76e0
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-07 20:25:47 +0100

    BetterThanAverage

---
+++ Codewars/BetterThanAverage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class BetterThanAverage
+    {
+        public static bool BetterThanAverages(int[] ClassPoints, int YourPoints)
+        {
+            int sum = 0;
+            
+            for(int i = 0; i < ClassPoints.Length; i++)
+            {
+                sum = sum + ClassPoints[i];
+            }
+            double avg = sum/ClassPoints.Length;
+            if (avg > YourPoints)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+}
