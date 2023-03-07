48faecad50d174d8405b38428c785d29dc1b9baf
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-07 21:10:36 +0100

    UpdateLights

--- Codewars/UpdateLights.cs
+++ Codewars/UpdateLights.cs
@@ -8,5 +8,18 @@ namespace Codewars
 {
     internal class UpdateLights
     {
+        public static string UpdateLight(string current)
+        {
+            if(current=="green")
+            {
+                return "yellow";
+            }else if(current=="yellow")
+            {
+                return "red";
+            }else
+            {
+                return "green";
+            }
+        }
     }
 }
