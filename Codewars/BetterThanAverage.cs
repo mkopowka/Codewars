76c3603c431bb4c84aaac0c4f4d5b8fc7aeadd58
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-07 20:26:14 +0100

    BetterThanAverage v2

--- Codewars/BetterThanAverage.cs
+++ Codewars/BetterThanAverage.cs
@@ -10,19 +10,7 @@ namespace Codewars
     {
         public static bool BetterThanAverages(int[] ClassPoints, int YourPoints)
         {
-            int sum = 0;
-            
-            for(int i = 0; i < ClassPoints.Length; i++)
-            {
-                sum = sum + ClassPoints[i];
-            }
-            double avg = sum/ClassPoints.Length;
-            if (avg > YourPoints)
-            {
-                return false;
-            }
-            else
-                return true;
+            return YourPoints > ClassPoints.Average();
         }
     }
 }
