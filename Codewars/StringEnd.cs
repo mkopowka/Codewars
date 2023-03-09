f0218d9797738059b5f49888d992a39042503247
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-09 17:45:00 +0100

    StringEnd

---
+++ Codewars/StringEnd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class StringEnd
+    {
+        public static bool Solution(string str, string ending)
+        {
+            if (str.EndsWith(ending)) { return true; }
+            else return false;
+        }
+    }
+}
