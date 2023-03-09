caf65d88fa8fb1f2036d045ccce4ea86706f877e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-09 14:18:17 +0100

    Persistence

---
+++ Codewars/Persistences.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Persistences
+    {
+        public static int Persistence(long n)
+        {
+            int count = 0;
+
+            while (n >= 10)
+            {
+                long product = 1;
+                while (n > 0)
+                {
+                    long digit = n % 10;
+                    product *= digit;
+                    n /= 10;
+                }
+                n = product;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
