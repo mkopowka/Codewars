64154b105cffd1b39b112230e05f25722778dcad
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-09 13:35:56 +0100

    digPow

---
+++ Codewars/DigsPow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class DigsPow
+    {
+        public static long digPow(int n, int p)
+        {
+            int sum = 0;
+            int temp = n;
+            int digitCount = (int)Math.Floor(Math.Log10(n) + 1); 
+
+            for (int i = digitCount + p - 1; i >= p; i--)
+            {
+                int digit = temp % 10;
+                sum += (int)Math.Pow(digit, i);
+                temp /= 10;
+            }
+
+            if (sum % n == 0)
+            {
+                return sum / n;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
