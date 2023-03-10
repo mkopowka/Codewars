89fe9d7b270e65d23c4b8c501a17ca3b00b03c53
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-10 20:28:24 +0100

    DescendingOrder

---
+++ Codewars/DescendingOrders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class DescendingOrders
+    {
+        public static int DescendingOrder(int num)
+        {
+            if(num == 0) return 0;
+            List<int> digits = new List<int>();
+            while (num > 0)
+            {
+                digits.Add(num % 10);
+                num /= 10;
+            }
+            digits.Sort();
+            digits.Reverse();
+            return int.Parse(string.Join("", digits));
+        }
+    }
+}
