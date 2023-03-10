95d74c14429ae53044b8928df8be97b3c933f630
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-10 20:13:01 +0100

    RowSumOddNumbers

---
+++ Codewars/RowSumOddNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class RowSumOddNumber
+    {
+        public static long RowSumOddNumbers(long n)
+        {
+            
+            long firstNumber = (n * (n - 1)) + 1;
+            long sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += firstNumber + (2 * i);
+            }
+
+            return sum;
+        }
+    }
+}
