fbee3b18cc36021eb32314181ecfde2f1a321a74
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-11 13:23:24 +0100

    DoubleChars

---
+++ Codewars/DoubleChars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class DoubleChars
+    {
+        public static string DoubleChar(string s)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                result.Append(c);
+                result.Append(c);
+            }
+
+            return result.ToString();
+        }
+    }
+}
