3a097417d6d4c84bb2e372ed6874c34bcc519dfe
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-11 09:59:31 +0100

    CountBy

---
+++ Codewars/CountsBy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class CountsBy
+    {
+        public int[] CountBy(int x, int n)
+        {
+            int[] z = new int[n];
+
+            for(int i = 0; i < n;i++)
+            {
+                z[i] = x * (i+1);
+            }
+
+            return z;
+        }
+    }
+}
