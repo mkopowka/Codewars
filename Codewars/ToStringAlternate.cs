1c430e011389f2a76488c1af362c32b0ccf65d60
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-12 20:42:51 +0100

    ToStringAlternate

---
+++ Codewars/ToStringAlternate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ToStringAlternate
+    {
+        public string ToAlternatingCases(string s)
+        {
+            var result = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (char.IsLower(c))
+                {
+                    result.Append(char.ToUpper(c));
+                }
+                else if (char.IsUpper(c))
+                {
+                    result.Append(char.ToLower(c));
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
