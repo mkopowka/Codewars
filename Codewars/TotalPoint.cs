1468050c58ab5c4befd8dd82e969cd37e3b9800d
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-12 12:11:20 +0100

    TotalPoints

---
+++ Codewars/TotalPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class TotalPoint
+    {
+        public static int TotalPoints(string[] games)
+        {
+            int totalPoints = 0;
+            foreach (string matchResult in games)
+            {
+                int x = int.Parse(matchResult.Split(':')[0]);
+                int y = int.Parse(matchResult.Split(':')[1]);
+
+                if (x > y)
+                {
+                    totalPoints += 3;
+                }
+                else if (x == y)
+                {
+                    totalPoints += 1;
+                }
+            }
+
+            return totalPoints;
+        }
+    }
+}
