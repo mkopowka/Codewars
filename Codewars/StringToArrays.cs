fcc46cf703d9e3898cf00d5f56abb8c602ed0e41
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-12 12:08:46 +0100

    StringToArray

--- Codewars/StringToArrays.cs
+++ Codewars/StringToArrays.cs
@@ -8,5 +8,17 @@ namespace Codewars
 {
     internal class StringToArrays
     {
+        public static string[] StringToArray(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return new string[0];
+            }
+            else
+            {
+                string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                return words.Length == 0 ? words : words.Select(w => w.Trim()).ToArray();
+            }
+        }
     }
 }
