a4d758c19c0c5dc16d459cbe39f1cdd7c80bf16b
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-12 20:32:17 +0100

    StringToNumber

---
+++ Codewars/StringToNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class StringToNumbers
+    {
+        public static int StringToNumber(String str)
+        {
+
+            //TODO: Convert str into a number
+            return Int32.Parse(str);
+        }
+    }
+}
