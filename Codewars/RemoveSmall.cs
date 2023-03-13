ac35e8abae722b533001998e3429dde1147fc9b8
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-13 14:17:38 +0100

    RemoveSmallest

---
+++ Codewars/RemoveSmall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class RemoveSmall
+    {
+        public List<int> RemoveSmallest(List<int> numbers)
+        {
+            if(numbers.Count == 0)
+              {
+                return numbers;
+            }
+
+            int min = numbers.Min();
+            int minIndex = numbers.IndexOf(min);
+            return numbers.Where((val, idx) => idx != minIndex).ToList();
+        }
+    }
+}
