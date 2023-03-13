481c94977a7b25f90d86539f04ac5b1519542145
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-13 14:00:08 +0100

    Past

---
+++ Codewars/Pasts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Pasts
+    {
+        public static int Past(int h, int m, int s)
+        {
+            return h* 3600000 + m * 60000 + s * 1000;
+        }
+    }
+}
