df8db0459c661517323cd08023ea8eedffdd1755
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-15 22:00:00 +0100

    Disemvowel

--- Codewars/Disemvowels.cs
+++ Codewars/Disemvowels.cs
@@ -8,5 +8,20 @@ namespace Codewars
 {
     internal class Disemvowels
     {
+        public static string Disemvowel(string str)
+        {
+            string vowels = "aeiouAEIOU";
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in str)
+            {
+                if (!vowels.Contains(c))
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
     }
 }
