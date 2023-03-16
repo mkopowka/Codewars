002e6d7736baa9210b6fa55609c56b7d218899a5
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-16 21:23:18 +0100

    Middle

--- Codewars/Middle.cs
+++ Codewars/Middle.cs
@@ -8,5 +8,19 @@ namespace Codewars
 {
     internal class Middle
     {
+        public static string GetMiddle(string s)
+        {
+            int length = s.Length;
+            int midIndex = length / 2;
+
+            if (length % 2 == 0)
+            {
+                return s.Substring(midIndex - 1, 2);
+            }
+            else
+            {
+                return s.Substring(midIndex, 1);
+            }
+        }
     }
 }
