97e95e299d06f800f897efaf3d934cb4a3c35e8d
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-16 21:23:07 +0100

    Enoughs

---
+++ Codewars/Enough.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Enough
+    {
+        public static int Enoughs(int cap, int on, int wait)
+        {
+            int availableSeats = cap - on;
+
+            if (availableSeats >= wait)
+            {
+                return 0;
+            }
+            else
+            {
+                return wait - availableSeats;
+            }
+        }
+    }
+}
