84be1d8e55521fe4295def79174050d1a30c5a77
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-23 18:00:18 +0100

    Digitize

--- Codewars/Digitizes.cs
+++ Codewars/Digitizes.cs
@@ -8,5 +8,22 @@ namespace Codewars
 {
     internal class Digitizes
     {
+        public static long[] Digitize(long n)
+        {
+            if (n == 0)
+            {
+                return new long[] { 0 };
+            }
+
+            List<long> digits = new List<long>();
+
+            while (n > 0)
+            {
+                digits.Add(n % 10);
+                n /= 10;
+            }
+
+            return digits.ToArray();
+        }
     }
 }
