63cc08b035501879f2f85742fb2fa0b7fda2767e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-27 17:40:19 +0200

    IsEven

---
+++ Codewars/IsEven.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class IsEven
+    {
+        public bool IsEvens(double n)
+        {
+            if (n % 2 == 0) { return true; } else return false;
+        }
+    }
+}
