8a9c82a23a9996b2af4ad19ccd73e87fdece9d73
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-28 16:20:05 +0200

    NumberPeople

---
+++ Codewars/NumberPeople.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class NumberPeople
+    {
+        public static int Number(List<int[]> peopleListInOut)
+        {
+            int count = 0;
+            foreach (int[] stop in peopleListInOut)
+            {
+                count += stop[0];  // add people who get on the bus
+                count -= stop[1];  // subtract people who get off the bus
+            }
+            return Math.Max(count, 0);
+        }
+    }
+}
