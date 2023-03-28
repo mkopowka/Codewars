c5b6fd7ab89c1bbb1a43c89e31daf2e349be587c
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-28 16:24:45 +0200

    Corrects

--- Codewars/Correct.cs
+++ Codewars/Correct.cs
@@ -8,5 +8,29 @@ namespace Codewars
 {
     internal class Correct
     {
+        public static string Corrects(string text)
+        {
+            string corrected = "";
+            foreach (char c in text)
+            {
+                if (c == '5')
+                {
+                    corrected += 'S';
+                }
+                else if (c == '0')
+                {
+                    corrected += 'O';
+                }
+                else if (c == '1')
+                {
+                    corrected += 'I';
+                }
+                else
+                {
+                    corrected += c;
+                }
+            }
+            return corrected;
+        }
     }
 }
