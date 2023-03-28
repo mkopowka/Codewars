df7e014335b7e5e09b454dd9b2b15d466df59365
Mateusz Kopówka <mkopowka@hotmail.com>
2023-03-28 20:32:28 +0200

    StrCounts

---
+++ Codewars/StrCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class StrCount
+    {
+        public static int StrCounts(string str, char letter)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == letter)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
