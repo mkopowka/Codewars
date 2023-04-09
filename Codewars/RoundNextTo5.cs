4c2d84ba4c7c90fdd3c1b199ef8dc18bebd082ef
Mateusz Kopówka <mkopowka@hotmail.com>
2023-04-09 19:35:07 +0200

    RoundNextTo5

---
+++ Codewars/RoundNextTo5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class RoundNextTo5
+    {
+        public static int RoundToNextMultipleOf5(int num)
+        {
+            return (num + 4) / 5 * 5;
+        }
+    }
+}
