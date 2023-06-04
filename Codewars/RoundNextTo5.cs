bed1a2830e91696f05b3f865669158803ecadc11
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-04 18:22:56 +0200

    RoundToNextMultipleOf5

--- Codewars/RoundNextTo5.cs
+++ Codewars/RoundNextTo5.cs
@@ -10,7 +10,7 @@ namespace Codewars
     {
         public static int RoundToNextMultipleOf5(int num)
         {
-
+            int n = 0;
             while (n % 5 != 0) n++;
             return n;
         }
