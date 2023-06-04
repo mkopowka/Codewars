ce7194efc4a3bcc654d788ca3b1b2ac2cb0a6295
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-04 19:27:17 +0200

    #fix

--- Codewars/IsIsogram.cs
+++ Codewars/IsIsogram.cs
@@ -8,5 +8,21 @@ namespace Codewars
 {
     internal class IsIsogram
     {
+        public static bool Isogram(string str)
+        {
+            str = str.ToLower();
+
+            HashSet<char> charSet = new HashSet<char>();
+
+            foreach (char c in str)
+            {
+                // If the character is a letter and already in the set, it's a repeating letter
+                if (char.IsLetter(c) && !charSet.Add(c))
+                    return false;
+            }
+
+            return true;
+
+        }
     }
 }
