12c9c471700f2e212d1d6268f2b53ba4a236bd54
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-05 11:05:10 +0200

    FindMultiple

---
+++ Codewars/FindMultiples.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FindMultiples
+    {
+        public List<int> FindMultiple(int integer, int limit)
+        {
+            List<int> values = new List<int>();
+            for(int i=integer;i<=limit;i+=integer)
+            {
+                values.Add(i);
+            }
+            return values;
+        }
+    }
+}
