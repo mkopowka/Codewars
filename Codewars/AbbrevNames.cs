f82ad25ddf431163c48b04dc283d7c675642b778
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-05 11:15:42 +0200

    Abbrev

--- Codewars/AbbrevNames.cs
+++ Codewars/AbbrevNames.cs
@@ -8,5 +8,17 @@ namespace Codewars
 {
     internal class AbbrevNames
     {
+        public static string AbbrevName(string name)
+        {
+            string[] words = name.Split(' ');
+
+
+            char firstInitial = char.ToUpper(words[0][0]);
+            char secondInitial = char.ToUpper(words[1][0]);
+
+            string initials = $"{firstInitial}.{secondInitial}";
+
+            return initials;
+        }
     }
 }
