2e0ca17b6c7235b57f0b72d8ce7af97d63482872
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-07 16:11:27 +0200

    ReverseLetters

--- Codewars/ReverseLetters.cs
+++ Codewars/ReverseLetters.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Codewars
 {
     internal class ReverseLetters
     {
+        public string ReverseLetter(string str)
+        {
+            string result = Regex.Replace(str, "[^a-zA-Z]", "");
+            char[] charArray = result.ToCharArray();
+
+            Array.Reverse(charArray);
+            string reverse = new string(charArray);
+
+            return reverse;
+        }
     }
 }
