8211204c575bd1521496559a3465a7729748451e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-07 16:17:07 +0200

    CountDuplicates

---
+++ Codewars/CountDuplicates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class CountDuplicates
+    {
+        public static int DuplicateCount(string str)
+        {
+            return -1;
+        }
+    }
+}
