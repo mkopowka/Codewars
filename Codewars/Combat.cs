0f5dbd77a950020ef82bc6a5b0a7b3f021667a1f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-07 16:11:13 +0200

    Combat

--- Codewars/Combat.cs
+++ Codewars/Combat.cs
@@ -8,5 +8,9 @@ namespace Codewars
 {
     internal class Combat
     {
+        public static float Combats(float health, float damage)
+        {
+            if(health-damage < 0) { return 0; } else return health- damage;
+        }
     }
 }
