37b473b3cf3e295568db262c3fd46d5df3804366
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-08 18:21:36 +0200

    VowelRemover

--- Codewars/VowelRemover.cs
+++ Codewars/VowelRemover.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Codewars
 {
     internal class VowelRemover
     {
+        public static string Shortcut(string input)
+        {
+            return Regex.Replace(input, "[aeiouAEIOU]", "");
+        }
     }
 }
