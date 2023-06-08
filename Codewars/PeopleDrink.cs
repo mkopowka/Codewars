22bc3007074d57d4b42a0544612cf3652af6887f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-08 18:21:32 +0200

    PeopleWithAgeDrink

---
+++ Codewars/PeopleDrink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class PeopleDrink
+    {
+        public static string PeopleWithAgeDrink(int old)
+        {
+            if (old < 14) { return "drink toddy"; }
+            else if (old >= 14 && old < 18) { return "drink coke"; }
+            else if (old >= 18 && old < 21) { return "drink beer"; }
+            else return "drink whisky";
+        }
+    }
+}
