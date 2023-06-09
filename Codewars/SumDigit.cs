18a550cb9b06a725c7004ac6d2772cf9fd0611e3
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-09 20:25:31 +0200

    SumDigits

---
+++ Codewars/SumDigit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumDigit
+    {
+        public static int SumDigits(int number)
+        {
+            int sum = 0;
+            number = Math.Abs(number);
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
