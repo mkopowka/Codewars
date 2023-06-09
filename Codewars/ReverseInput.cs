5bab8a7419d2daa0e2e2785f197558aa3f6287ec
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-09 20:22:33 +0200

    ReverseInput

---
+++ Codewars/ReverseInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ReverseInput
+    {
+        public static string Reverse(string text)
+        {
+            text = text.Trim();
+            string[] words = Regex.Split(text, @"(\s+)");
+
+            Array.Reverse(words);
+            string reversed = string.Concat(words);
+
+            return reversed;
+        }
+    }
+}
