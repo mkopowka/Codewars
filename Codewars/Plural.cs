427797c8ca63c4d155d4332f2672364fe735df94
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-09 20:24:21 +0200

    Plurals

---
+++ Codewars/Plural.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Plural
+    {
+        public static bool Plurals(double n)
+        {
+            return n != 1;
+        }
+    }
+}
