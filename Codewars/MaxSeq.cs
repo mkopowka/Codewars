fe05ad4fb0d8756eeec813acc6b19dcc1cf610f5
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-10 19:34:19 +0200

    MaxSequence

---
+++ Codewars/MaxSeq.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class MaxSeq
+    {
+        public static int MaxSequence(int[] arr)
+        {
+            if (arr.Length == 0)
+                return 0;
+
+            int maxSum = arr[0];
+            int currentSum = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                currentSum = Math.Max(arr[i], currentSum + arr[i]);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
