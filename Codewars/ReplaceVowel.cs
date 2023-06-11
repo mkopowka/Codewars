3be67abd93a9ed9689dafaa9ed3d00375a5504f9
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-11 21:37:34 +0200

    ReplaceVowel

---
+++ Codewars/ReplaceVowel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ReplaceVowel
+    {
+        public static string Replace(string s)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            foreach (char vowel in vowels)
+            {
+                s = s.Replace(vowel, '!');
+            }
+
+            return s;
+        }
+    }
+}
