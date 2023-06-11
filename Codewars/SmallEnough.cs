3c598d27cfb96b143a7045b385dab7d308628739
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-11 21:36:03 +0200

    SmallEnoughs

---
+++ Codewars/SmallEnough.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SmallEnough
+    {
+        public bool SmallEnoughs(int[] a, int limit)
+        {
+
+            foreach (int value in a)
+            {
+                if (value > limit)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
