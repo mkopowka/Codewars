2aec128ce505fb4b20cecf04731df3a30bd46990
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-11 21:26:57 +0200

    PipeFix

---
+++ Codewars/PipeFix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class PipeFix
+    {
+        public static List<int> PipeFixer(List<int> numbers)
+        {
+            List<int> result = new List<int>();
+            for (int i = numbers.FirstOrDefault(); i < numbers.LastOrDefault()+!; i++) 
+            {
+                result.Add(i);
+            }
+
+            return result;
+        }
+    }
+}
