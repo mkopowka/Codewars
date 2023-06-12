4d750c7d6a225e7f7af24c73286e9e4610cf98c6
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-12 13:58:13 +0200

    ReverseList

---
+++ Codewars/ReverseList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ReverseList
+    {
+        public static List<int> ReverseLists(List<int> list)
+        {
+            List<int> reversedList = Enumerable.Reverse(list).ToList();
+            return reversedList;
+        }
+    }
+}
