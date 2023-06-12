66bf76c039de783f742c19a189f2651ce51c2138
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-12 14:06:57 +0200

    NoBoringZeros

---
+++ Codewars/NoZeros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class NoZeros
+    {
+        public static int NoBoringZeros(int number)
+        {
+            if (number == 0)
+                return 0;
+
+            while (number % 10 == 0)
+            {
+                number /= 10;
+            }
+
+            return number;
+        }
+    }
+}
