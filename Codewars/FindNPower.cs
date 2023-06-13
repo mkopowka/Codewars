ef51d245c2d1d90e69eb70e69a87d0052157b78a
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-13 16:54:37 +0200

    FindNPower

---
+++ Codewars/FindNPower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FindNPower
+    {
+        public static double Index(int[] array, int n)
+        {
+            if (n >= 0 && n < array.Length)
+            {
+                int element = array[n];
+                return (long)Math.Pow(element, n);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
