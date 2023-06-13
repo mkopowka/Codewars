fe544c13ceeef14aa944f123d500a236008bef5b
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-13 16:34:46 +0200

    GameMove

---
+++ Codewars/GameMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GameMove
+    {
+        public static int Move(int position, int roll)
+        {
+            return position + (roll * 2);
+        }
+    }
+}
