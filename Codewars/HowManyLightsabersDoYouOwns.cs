c2e81bd7cf6f36ec91cbb6c1fc2e04dbef67f381
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-15 20:53:07 +0200

    HowManyLightsabersDoYouOwn

--- Codewars/HowManyLightsabersDoYouOwns.cs
+++ Codewars/HowManyLightsabersDoYouOwns.cs
@@ -8,5 +8,10 @@ namespace Codewars
 {
     internal class HowManyLightsabersDoYouOwns
     {
+        public static int HowManyLightsabersDoYouOwn(string name)
+        {
+            if (name == "Zach") return 18;
+            else return 0;
+        }
     }
 }
