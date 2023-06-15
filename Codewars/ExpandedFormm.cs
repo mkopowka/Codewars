519bfc473fc860887339e498c8fc280728fd52ab
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-15 20:53:22 +0200

    ExpandedForm

--- Codewars/ExpandedFormm.cs
+++ Codewars/ExpandedFormm.cs
@@ -8,5 +8,24 @@ namespace Codewars
 {
     internal class ExpandedFormm
     {
+        public static string ExpandedForm(int num)
+        {
+            string number = num.ToString();
+            string expandedForm = "";
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] != '0')
+                {
+                    int zeros = number.Length - i - 1;
+                    expandedForm += number[i] + new string('0', zeros) + " + ";
+                }
+            }
+
+            // Remove the trailing " + " from the expanded form
+            expandedForm = expandedForm.TrimEnd(' ', '+');
+
+            return expandedForm;
+        }
     }
 }
