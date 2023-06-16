dc4d0301804094654cf1bd1cbed3a3a2825afae3
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-16 17:03:31 +0200

    ReverseWord

---
+++ Codewars/ReverseWords.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ReverseWords
+    {
+        public static string ReverseWord(string str)
+        {
+            string[] words = str.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] chars = words[i].ToCharArray();
+                Array.Reverse(chars);
+                words[i] = new string(chars);
+            }
+
+            string reversedSentence = string.Join(" ", words);
+
+            return reversedSentence;
+        }
+    }
+}
