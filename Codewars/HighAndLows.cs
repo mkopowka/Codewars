e9d2e94fab3fbd0c143ee514734a7ac528000b40
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-16 17:12:26 +0200

    GetMaxAndMin

---
+++ Codewars/HighAndLows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class HighAndLows
+    {
+        public static string GetMaxAndMin(string numbersString)
+        {
+            string[] numberStrings = numbersString.Split(' ');
+
+            int[] numbers = numberStrings.Select(int.Parse).ToArray();
+
+            int max = numbers.Max();
+            int min = numbers.Min();
+
+            return $"{max} {min}";
+        }
+    }
+}
