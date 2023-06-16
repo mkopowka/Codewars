ca36a550caebbf7eb78a21c2272a466666532adf
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-16 17:18:03 +0200

    SwapFirstNameLastName

---
+++ Codewars/SwapFirstNameLastName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SwapFirstNameLastName
+    {
+        public static string NameShuffler(string str)
+        {
+            string[] nameParts = str.Split(' ');
+            string firstName = nameParts[0];
+            string lastName = nameParts[1];
+            string swappedName = $"{lastName} {firstName}";
+
+            return swappedName;
+        }
+    }
+}
