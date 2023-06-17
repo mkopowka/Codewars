7ee39f6eb68992eabbebaed1a03d59b7b58e0670
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-17 14:51:13 +0200

    FindNextSquare

---
+++ Codewars/FindNextSquare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FindNextSquares
+    {
+        public static long FindNextSquare(long num)
+        {
+            double final = Math.Sqrt(num);
+            bool isSqr = final % 1 == 0;
+            if (isSqr == true)
+            {
+                final++;
+                final = final * final;
+                long fi = Convert.ToInt64(final);
+                return fi;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+}
