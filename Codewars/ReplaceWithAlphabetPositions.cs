570a245e3168c51bbee0470a962f79e52208ab8d
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-17 14:44:37 +0200

    ReplaceWithAlphabetPositions

--- Codewars/ReplaceWithAlphabetPositions.cs
+++ Codewars/ReplaceWithAlphabetPositions.cs
@@ -8,5 +8,28 @@ namespace Codewars
 {
     internal class ReplaceWithAlphabetPositions
     {
+        public static string AlphabetPosition(string text)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char uppercaseChar = char.ToUpper(c);
+                    int position = uppercaseChar - 'A' + 1;
+                    result.Append(position);
+                    result.Append(" ");
+                }
+            }
+
+            if (result.Length > 0)
+            {
+                // Remove the trailing space
+                result.Length--;
+            }
+
+            return result.ToString();
+        }
     }
 }
