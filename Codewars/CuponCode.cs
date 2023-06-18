c1f571bcc1958025d3a14b2748e36c559b6a7fb2
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-18 19:04:08 +0200

    CuponCode

--- Codewars/CuponCode.cs
+++ Codewars/CuponCode.cs
@@ -10,28 +10,7 @@ namespace Codewars
     {
         public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
         {
-            if(enteredCode!=correctCode) { return false; }
-
-            DateTime parsedCurrentDate, parsedExpirationDate;
-
-            if (!DateTime.TryParse(currentDate, out parsedCurrentDate) || !DateTime.TryParse(expirationDate, out parsedExpirationDate))
-            {
-                Console.WriteLine("Invalid date format.");
-                return false;
-            }
-
-            parsedExpirationDate = parsedExpirationDate.AddDays(1).Date;
-
-            if (parsedCurrentDate <= parsedExpirationDate)
-            {
-                Console.WriteLine("Coupon is valid!");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Coupon has expired.");
-                return false;
-            }
+            return enteredCode == correctCode && DateTime.Parse(currentDate) <= DateTime.Parse(expirationDate);
         }
     }
 }
