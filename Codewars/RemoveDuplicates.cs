199d368cedb0ca6b73af7c9d8422215646c76d5e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-18 19:06:05 +0200

    RemoveDuplicates

---
+++ Codewars/RemoveDuplicates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class RemoveDuplicates
+    {
+        public static int[] distinct(int[] a)
+        {
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+            List<int> result = new List<int>();
+
+            foreach (int number in a)
+            {
+                if (uniqueNumbers.Add(number))
+                {
+                    result.Add(number);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
