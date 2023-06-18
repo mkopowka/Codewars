7d8f816f66a61215c53ff85ab139c1cdaf4bc24f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-18 18:58:43 +0200

    MouthSize

---
+++ Codewars/MouthSizes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class MouthSizes
+    {
+        public static string MouthSize(string animal)
+        {
+            if (animal.ToLower() == "alligator") return "small";
+            else return "wide";
+        }
+    }
+}
