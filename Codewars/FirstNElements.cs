8abec55198b76d050212fa99c686f97ec15bbe9e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-19 17:32:41 +0200

    FirstNElements

---
+++ Codewars/FirstNElements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FirstNElements
+    {
+        public static int[] Take(int[] arr, int n)
+        {
+            if (n >= arr.Length)
+            {
+                return arr;
+            }
+            else
+            {
+                int[] result = new int[n];
+                Array.Copy(arr, result, n);
+                return result;
+            }
+        }
+    }
+}
