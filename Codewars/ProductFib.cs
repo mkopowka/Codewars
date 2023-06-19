889b2583969361635e7dbf34387d33312e290019
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-19 17:40:02 +0200

    ProductFibs

---
+++ Codewars/ProductFib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ProductFib
+    {
+        public static ulong[] ProductFibs(ulong prod)
+        {
+            ulong fib1 = 0;
+            ulong fib2 = 1;
+
+            while (fib1 * fib2 < prod)
+            {
+                ulong temp = fib1;
+                fib1 = fib2;
+                fib2 = temp + fib2;
+            }
+
+            return new ulong[] { fib1, fib2, (ulong)(fib1 * fib2 == prod ? 1 : 0) };
+        }
+    }
+}
