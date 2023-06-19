c875372aa95434966dce40db36a7ed472c5b6615
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-19 17:41:43 +0200

    GetChar

---
+++ Codewars/GetChars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetChars
+    {
+        public static char GetChar(int charcode)
+        {
+            return (char)charcode;
+  }
+    }
+}
