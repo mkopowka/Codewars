473abfa5e4a7f5350504ae91fadb3f3bce20faea
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-20 15:14:37 +0200

    Comps

--- Codewars/Comp.cs
+++ Codewars/Comp.cs
@@ -10,39 +10,7 @@ namespace Codewars
     {
         public static bool Comps(int[] a, int[] b)
         {
-            if (a == null || b == null || a.Length != b.Length)
-                return false;
-
-            var freqA = new Dictionary<int, int>();
-            var freqB = new Dictionary<int, int>();
-
-            foreach (int num in a)
-            {
-                if (freqA.ContainsKey(num))
-                    freqA[num]++;
-                else
-                    freqA[num] = 1;
-            }
-
-            foreach (int num in b)
-            {
-                if (freqB.ContainsKey(num))
-                    freqB[num]++;
-                else
-                    freqB[num] = 1;
-            }
-
-            foreach (var kvp in freqA)
-            {
-                int num = kvp.Key;
-                int freqInA = kvp.Value;
-
-                if (!freqB.ContainsKey(num * num) || freqB[num * num] != freqInA)
-                    return false;
-            }
-
-
-            return true;
+            return a != null && b != null && a.Select(v => v * v).OrderBy(v => v).SequenceEqual(b.OrderBy(v => v));
         }
     }
 }
