36fab2b010aa5006fefbb082064093874e274b46
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-21 16:32:24 +0200

    Between

---
+++ Codewars/Betweens.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Betweens
+    {
+        public static int[] Between(int a, int b)
+        {
+            int length = b - a + 1;
+            int[] result = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = a + i;
+            }
+
+            return result;
+        }
+    }
+}
