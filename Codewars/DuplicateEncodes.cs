93b6e1c9dc5d4362e88b3cdc60b380402e779c7f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-22 21:28:44 +0200

    DuplicateEncode(string

--- Codewars/DuplicateEncodes.cs
+++ Codewars/DuplicateEncodes.cs
@@ -8,5 +8,18 @@ namespace Codewars
 {
     internal class DuplicateEncodes
     {
+        public static string DuplicateEncode(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+                return word;
+
+            var lowercaseWord = word.ToLower();
+            var charCounts = lowercaseWord.GroupBy(c => c)
+                                          .ToDictionary(g => g.Key, g => g.Count());
+
+            var result = string.Concat(lowercaseWord.Select(c => charCounts[c] > 1 ? ')' : '('));
+
+            return result;
+        }
     }
 }
