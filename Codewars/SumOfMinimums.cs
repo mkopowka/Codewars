79bdc567e54b1afd06b0d8662bf73c7aed5a6276
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-23 19:39:21 +0200

    SumOfMinimum

---
+++ Codewars/SumOfMinimums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumOfMinimum
+    {
+        public static int SumOfMinimums(int[,] numbers)
+        {
+            int sum = 0;
+            int rows = numbers.GetLength(0);
+            int columns = numbers.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                int min = int.MaxValue;
+                for (int j = 0; j < columns; j++)
+                {
+                    if (numbers[i, j] < min)
+                    {
+                        min = numbers[i, j];
+                    }
+                }
+                sum += min;
+            }
+
+            return sum;
+        }
+    }
+}
