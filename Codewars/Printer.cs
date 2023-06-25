31e1bb3385df282a85fbd41e29a98a0a493dcf4b
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-25 20:43:41 +0200

    PrinterError

---
+++ Codewars/Printer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Printer
+    {
+        public static string PrinterError(String s)
+        {
+            int errorCount = 0;
+            int totalLength = s.Length;
+
+            foreach (char c in s)
+            {
+                if (c < 'a' || c > 'm')
+                {
+                    errorCount++;
+                }
+            }
+            return $"{errorCount}/{totalLength}";
+        }
+    }
+}
