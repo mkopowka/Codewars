8761bdbda210f131a8903d77d277a6f72cc62660
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-25 20:31:02 +0200

    Includes

---
+++ Codewars/Include.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Include
+    {
+        public static bool Includes(int[] arr, int item)
+        {
+            if (arr.Contains(item)) return true;
+            else return false;
+        }
+    }
+}
