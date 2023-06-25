0b13d374517a60070eae8643a665b038c279ee07
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-25 20:28:57 +0200

    Balls

---
+++ Codewars/Balls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Balls
+    {
+        public string ballType { get; set; }
+  
+    public Balls(string ballType = "regular")
+    {
+        this.ballType = ballType;
+    }
+
+    // Method to get the ball type
+    public string GetBallType()
+    {
+        return ballType;
+    }
+    }
+}
