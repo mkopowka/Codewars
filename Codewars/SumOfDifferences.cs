20ce6b90cbf54cd438502252999a2eae6cae4a8b
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-26 20:37:12 +0200

    SumOfDifferences

---
+++ Codewars/SumOfDifferences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumOfDifferences
+    {
+        public static int SumDifferencesDescending(int[] array)
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            int sum = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                sum += array[i] - array[i + 1];
+            }
+
+            return sum;
+        }
+    }
+}
