d47c2b900fc2a43beca0b0aab0d1d181fc8213e6
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-26 20:32:17 +0200

    RepeatStr

---
+++ Codewars/StringRepeater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class StringRepeater
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            return String.Concat(Enumerable.Repeat(s, n));
+        }
+    }
+}
