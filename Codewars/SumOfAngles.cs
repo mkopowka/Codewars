e0c2ed6436d0de08fb58bf9b47e0a7ec4fe46888
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-26 20:35:13 +0200

    SumOfAngles

---
+++ Codewars/SumOfAngles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumOfAngles
+    {
+        public static int Angle(int n)
+        {
+            return (n = -2) * 180;
+        }
+    }
+}
