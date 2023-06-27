258d30814d79532c3681362e8669195cbf5e6a4c
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-27 17:41:49 +0200

    GetDrinkByProfessions

---
+++ Codewars/GetDrinkByProfession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetDrinkByProfession
+    {
+        public static string GetDrinkByProfessions(string p)
+        {
+            string lowercaseInput = p.ToLower();
+
+            switch (lowercaseInput)
+            {
+                case "jabroni":
+                    return "Patron Tequila";
+                case "school counselor":
+                    return "Anything with Alcohol";
+                case "programmer":
+                    return "Hipster Craft Beer";
+                case "bike gang member":
+                    return "Moonshine";
+                case "politician":
+                    return "Your tax dollars";
+                case "rapper":
+                    return "Cristal";
+                default:
+                    return "Beer";
+            }
+        }
+    }
+}
