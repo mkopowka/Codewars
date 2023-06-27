82baa451ee058312250adba9c03020cb87fa51f9
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-27 17:44:44 +0200

    IsUpperCase

---
+++ Codewars/IsUpperCases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal static class IsUpperCases
+    {
+        public static bool IsUpperCase(this string text)
+        {
+            return text.Equals(text.ToUpper());
+        }
+    }
+}
