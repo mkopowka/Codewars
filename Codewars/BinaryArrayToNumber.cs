cf218297b84578c0e7afd1c00612852e2e4beaca
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-27 17:48:17 +0200

    binaryArrayToNumbers

---
+++ Codewars/BinaryArrayToNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class BinaryArrayToNumber
+    {
+        public static int binaryArrayToNumbers(int[] BinaryArray)
+        {
+            string binaryString = string.Join("", BinaryArray);
+            int decimalValue = Convert.ToInt32(binaryString, 2);
+            return decimalValue;
+        }
+    }
+}
