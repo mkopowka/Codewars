60a47796a2599d74a4fea84504331861a661cb84
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-28 16:40:42 +0200

    MexicanaWave

---
+++ Codewars/MexicanaWave.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class MexicanaWave
+    {
+        public List<string> wave(string str)
+        {
+            List<string> waveList = new List<string>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsWhiteSpace(str[i]))
+                    continue;
+
+                string wave = str.Substring(0, i) + char.ToUpper(str[i]) + str.Substring(i + 1);
+                waveList.Add(wave);
+            }
+
+            return waveList;
+        }
+    }
+}
