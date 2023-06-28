150f336697db6d2f153b713ab31793cafefd35d4
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-28 16:26:40 +0200

    PowerOf2

---
+++ Codewars/PowerOf2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class PowerOf2
+    {
+        public static BigInteger[] PowersOfTwo(int n)
+        {
+            BigInteger[] powers = new BigInteger[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                powers[i] = BigInteger.Pow(2, i);
+            }
+
+            return powers;
+        }
+    }
+}
