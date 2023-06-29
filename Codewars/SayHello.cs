b4fd4e44dc7d2126cc6b074cb79bfd314f8273a0
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-29 15:05:22 +0200

    SayHello

---
+++ Codewars/SayHello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SayHello
+    {
+        public static string SayHellos(string[] name, string city, string state)
+        {
+            string fullName = string.Join(" ", name);
+            string greeting = $"Hello, {fullName}! Welcome to {city}, {state}!";
+
+            return greeting;
+        }
+    }
+}
