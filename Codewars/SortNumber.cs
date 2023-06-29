119f155efdfc69d7d0883f16e9ebfe6a0397736c
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-29 15:02:45 +0200

    SortNumber

---
+++ Codewars/SortNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SortNumber
+    {
+        public int[] SortNumbers(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return new int[0];
+            }
+
+            Array.Sort(nums);
+
+            return nums;
+
+        }
+
+    }
+}
