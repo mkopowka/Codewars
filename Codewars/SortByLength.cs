a3402d841dfe629742b9266cbf834d00b4454ee2
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-30 19:56:18 +0200

    SortByLengths(string[] array)         {

---
+++ Codewars/SortByLength.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SortByLength
+    {
+        public static string[] SortByLengths(string[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                string temp = array[i];
+
+                int j = i - 1;
+                while (j >= 0 && temp.Length < array[j].Length)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
+            }
+            return array;
+        }
+    }
+}
