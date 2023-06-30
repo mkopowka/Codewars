dfb40f880156748c337591d262d73ec95455d15e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-06-30 19:51:32 +0200

    Getsize

---
+++ Codewars/GetSize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetSize
+    {
+        public static int[] Getsize(int w, int h, int d)
+        {
+            int surfaceArea = (2 * (w * h)) + (2 * (w * d)) + (2 * (h * d));
+            int volume = w * h * d;
+
+            return new int[] { surfaceArea, volume };
+        }
+    }
+}
