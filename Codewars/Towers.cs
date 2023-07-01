e13c14e30d21f92aaceb3445c5c77526a2e9411a
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-01 14:22:50 +0200

    Towers

---
+++ Codewars/Towers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Towers
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            List<string> tower = new List<string>();
+
+            int width = 2 * nFloors - 1;
+            for (int i = 0; i < nFloors; i++)
+            {
+                int numStars = 2 * i + 1;
+                int numSpaces = (width - numStars) / 2;
+
+                string floor = new string(' ', numSpaces) + new string('*', numStars) + new string(' ', numSpaces);
+                tower.Add(floor);
+            }
+
+            return tower.ToArray();
+        }
+    }
+}
