0bf17fc0b04324e397e66859ebd2d84041d3a414
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-01 14:20:37 +0200

    Diamonds

---
+++ Codewars/Diamonds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Diamonds
+    {
+        public static string print(int n)
+        {
+            if (n % 2 == 0 || n < 0)
+            {
+                return null;
+            }
+
+            StringBuilder diamond = new StringBuilder();
+
+            // Upper half of the diamond
+            for (int i = 0; i < n / 2 + 1; i++)
+            {
+                diamond.Append(new string(' ', n / 2 - i));
+                diamond.Append(new string('*', 2 * i + 1));
+                diamond.Append('\n');
+            }
+
+            // Lower half of the diamond
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                diamond.Append(new string(' ', n / 2 - i));
+                diamond.Append(new string('*', 2 * i + 1));
+                diamond.Append('\n');
+            }
+
+            return diamond.ToString();
+        }
+    }
+}
