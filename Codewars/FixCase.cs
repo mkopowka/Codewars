624d556d46ccdabcf78948eff1d4b55a806dba47
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-02 17:39:11 +0200

    FixCase

---
+++ Codewars/FixCase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FixCase
+    {
+        public static string Solve(string input)
+        {
+            int uppercaseCount = 0;
+            int lowercaseCount = 0;
+
+            // Count the number of uppercase and lowercase letters
+            foreach (char c in input)
+            {
+                if (char.IsUpper(c))
+                {
+                    uppercaseCount++;
+                }
+                else if (char.IsLower(c))
+                {
+                    lowercaseCount++;
+                }
+            }
+
+            // Convert the string based on the conditions
+            if (uppercaseCount > lowercaseCount)
+            {
+                return input.ToUpper();
+            }
+            else
+            {
+                return input.ToLower();
+            }
+        }
+    }
+}
