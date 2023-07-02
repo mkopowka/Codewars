7dee02711be98df00d28923cd1a4df2b8136d5ce
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-02 17:43:00 +0200

    Binary

---
+++ Codewars/Binary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Binary
+    {
+        public static string AddBinary(int a, int b)
+        {
+            int sum = a + b;
+            string binary = Convert.ToString(sum, 2);
+            return binary;
+        }
+    }
+}
