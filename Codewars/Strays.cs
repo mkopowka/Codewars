85fd7ec0a49ac8a8286eb60d3e0e33c33b215de0
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-03 17:35:14 +0200

    Strays

---
+++ Codewars/Strays.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Strays
+    {
+        public static int Stray(int[] numbers)
+        {
+            int result = 0;
+            foreach (int num in numbers)
+            {
+                result ^= num;
+            }
+            return result;
+        }
+    }
+}
