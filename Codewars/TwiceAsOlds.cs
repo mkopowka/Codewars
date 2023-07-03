69a8a3d102dc0b01dbdc141aaf18acdf5394da09
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-03 17:42:19 +0200

    TwiceAsOlds

---
+++ Codewars/TwiceAsOlds.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class TwiceAsOlds
+    {
+        public static int TwiceAsOld(int dadYears, int sonYears)
+        {
+            int yearsAgo = Math.Abs(dadYears - 2 * sonYears);
+            return yearsAgo;
+        }
+    }
+}
