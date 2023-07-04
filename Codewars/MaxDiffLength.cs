0778014a5d18b0d6024ab9c71a49423aae2ee749
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-04 16:35:17 +0200

    MaxDiffLength

---
+++ Codewars/MaxDiffLength.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class MaxDiffLength
+    {
+        public static int Mxdiflg(string[] a1, string[] a2)
+        {
+            if (a1.Length == 0 || a2.Length == 0)
+                return -1;
+
+            int maxLengthDiff = int.MinValue;
+
+            foreach (string x in a1)
+            {
+                foreach (string y in a2)
+                {
+                    int lengthDiff = Math.Abs(x.Length - y.Length);
+                    if (lengthDiff > maxLengthDiff)
+                        maxLengthDiff = lengthDiff;
+                }
+            }
+
+            return maxLengthDiff;
+        }
+    }
+}
