ff769b8e72ca6abddd10e07bff87b7e8258455e1
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-04 16:37:33 +0200

    BinaryToDecimal

---
+++ Codewars/BinToDecimal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class BinToDecimal
+    {
+        public static int BinaryToDecimal(string binary)
+        {
+            int decimalValue = Convert.ToInt32(binary, 2);
+            return decimalValue;
+        }
+    }
+}
