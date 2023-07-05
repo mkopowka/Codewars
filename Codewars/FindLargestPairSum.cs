101820a47c6d68a73edbd71a6ab76a2810c7c3a7
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-05 15:02:27 +0200

    LargestPairSum

---
+++ Codewars/FindLargestPairSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FindLargestPairSum
+    {
+        public static int LargestPairSum(int[] numbers)
+        {
+
+            int largestSum = int.MinValue;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int sum = numbers[i] + numbers[j];
+                    largestSum = Math.Max(largestSum, sum);
+                }
+            }
+
+            return largestSum;
+        }
+    }
+}
