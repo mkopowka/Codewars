1bc9fdb58d16456da4392d83a940dd84ef63c572
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-05 14:59:45 +0200

    LineNumbering

---
+++ Codewars/LineNumbering.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class LineNumbering
+    {
+        public static List<string> Number(List<string> lines)
+        {
+            List<string> numberedLines = new List<string>();
+            int lineNumber = 1;
+
+            foreach (string line in lines)
+            {
+                string numberedLine = $"{lineNumber}: {line}";
+                numberedLines.Add(numberedLine);
+                lineNumber++;
+            }
+
+            return numberedLines;
+        }
+    }
+}
