2bf12f19aa1658ec41d52053a3897bfee4074a99
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-06 15:26:56 +0200

    Array

---
+++ Codewars/Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Array
+    {
+        public static string Arrays(string s)
+        {
+
+            if (string.IsNullOrEmpty(s))
+                return null;
+
+            string[] sequences = s.Split(',');
+
+            if (sequences.Length <= 2)
+                return null;
+
+            return string.Join(" ", sequences[1..^1]);
+        }
+    }
+}
