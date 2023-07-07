4261d4c2b291749043147d1ce112672df0b270da
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-07 14:47:16 +0200

    CountCharacters

---
+++ Codewars/CountChar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class CountChar
+    {
+        public static Dictionary<char, int> CountCharacters(string input)
+        {
+            Dictionary<char, int> characterCounts = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (characterCounts.ContainsKey(c))
+                {
+                    characterCounts[c]++;
+                }
+                else
+                {
+                    characterCounts[c] = 1;
+                }
+            }
+
+            return characterCounts;
+        }
+    }
+}
