9477aba3bd5e470967f70dcc7d625654402bc587
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-08 17:55:36 +0200

    FixString

--- Codewars/FixString.cs
+++ Codewars/FixString.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Codewars
-{
-    internal class FixString
+{ 
+    internal static class FixString
     {
+        public static string a = "dev";
+        public static string b = "Lab";
+        public static string name =String.Concat(a + b);
     }
 }
