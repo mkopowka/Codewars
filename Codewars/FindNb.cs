47c66a36ed042d6a072c5b79b33b5a17124b6e3c
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-08 17:50:36 +0200

    findNbs

---
+++ Codewars/FindNb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FindNb
+    {
+        public static long findNbs(long m)
+        {
+            long sum = 0;
+            long n = 1;
+
+            while (sum < m)
+            {
+                sum += (long)Math.Pow(n, 3);
+                n++;
+            }
+
+            if (sum == m)
+            {
+                return n - 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+}
