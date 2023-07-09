2f1268d546a46cfd563e3717cd7080f214bbf282
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-09 14:01:22 +0200

    RemoveExclamationMarks

---
+++ Codewars/RemoveMarks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class RemoveMarks
+    {
+        public static string RemoveExclamationMarks(string s)
+        {
+            return s.Replace("!", "");
+        }
+    }
+}
