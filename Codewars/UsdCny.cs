0cc988fc3ffccbfa2d4731c984586f7690db5967
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-10 17:24:21 +0200

    Usdcny

---
+++ Codewars/UsdCny.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class UsdCny
+    {
+        public static string Usdcny(int usd)
+        {
+            double sum = usd * 6.75;
+   
+            return string.Format("{0:f2}", sum) + " Chinese Yuan";
+        }
+    }
+}
