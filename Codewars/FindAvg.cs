d9f7672c20a3e4e378f1b1653fb70677a2bec955
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-10 17:19:09 +0200

    FindAverage

---
+++ Codewars/FindAvg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FindAvg
+    {
+        public static int FindAverage(int[] nums)
+        {
+            int count = nums.Length;
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += nums[i];
+            }
+            sum /= count;
+            return sum;
+        }
+    }
+}
