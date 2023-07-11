c8d841c4526be4c6aa44c5233b773877bb5c546f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-11 21:38:21 +0200

    StayHydrated

---
+++ Codewars/StayHydrated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class StayHydrated
+    {
+        public static int Litres(double time)
+        {
+            double liter = time * 0.5;
+            return (int)liter;
+        }
+    }
+}
