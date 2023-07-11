0003df12b4dedb64870d16a77abf9795abcba6fb
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-11 16:20:29 +0200

    SaleHotDog

---
+++ Codewars/SaleHotDogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SaleHotDogs
+    {
+        public static int SaleHotDog(int n)
+        {
+            if (n < 5) { return 100 * n; }
+            else if (n >= 5 && n < 10) { return 95 * n; }
+            else return 90 * n;
+        }
+    }
+}
