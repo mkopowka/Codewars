bc34f1191b189a4d07842410f529f5649d345228
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-13 14:23:10 +0200

    GetUnique

---
+++ Codewars/GetUniques.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class GetUniques
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            Dictionary<int, int> countMap = new Dictionary<int, int>();
+
+            // Count the occurrences of each number
+            foreach (int num in numbers)
+            {
+                if (countMap.ContainsKey(num))
+                    countMap[num]++;
+                else
+                    countMap[num] = 1;
+            }
+
+            // Find the number with count 1
+            foreach (KeyValuePair<int, int> pair in countMap)
+            {
+                if (pair.Value == 1)
+                    return pair.Key;
+            }
+
+            return -1;
+        }
+    }
+}
