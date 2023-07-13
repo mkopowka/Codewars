75a564d140a26460cc689958ba044c8f88e39211
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-13 14:21:30 +0200

    DutyFrees

---
+++ Codewars/DutyFrees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class DutyFrees
+    {
+        public static int DutyFree(int normPrice, int Discount, int hol)
+        {
+            double savingsPerBottle = normPrice * (Discount / 100.0);
+            int bottlesNeeded = (int)(hol / savingsPerBottle);
+            return bottlesNeeded;
+        }
+    }
+}
