3828e1eabfdd34cc9ab6365496b9863910eee639
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-14 22:07:52 +0200

    DnaToRna

---
+++ Codewars/DnaToRna.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class DnaToRna
+    {
+        public string dnaToRna(string dna)
+        {
+            string rna = "";
+
+            foreach (char nucleotide in dna)
+            {
+                if (nucleotide == 'T')
+                {
+                    rna += 'U';
+                }
+                else
+                {
+                    rna += nucleotide;
+                }
+            }
+
+            return rna;
+        }
+    }
+}
