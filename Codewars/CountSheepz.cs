24513d1ba25b37ce7e714a80f995198a9c5eb28f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-07-30 21:07:31 +0200

    CountSheepz

---
+++ Codewars/CountSheepz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class CountSheepz
+    {
+        public static int CountSheeps(bool[] sheeps)
+        {
+            int count = 0;
+
+            for (int i = 0; i < sheeps.Length; i++)
+            {
+                if (sheeps[i] == true)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
