c17b18b53b2dcbec72ea3052444270df3eeb4edf
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-03 15:41:08 +0200

    SeriesSum

---
+++ Codewars/SeriesSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SeriesSum
+    {
+        public static string seriesSum(int n)
+        {
+            return (from i in Enumerable.Range(0, n) select 1.0 / (3 * i + 1)).Sum().ToString("0.00");
+        }
+    }
+}
