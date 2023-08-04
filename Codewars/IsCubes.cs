d301c4e6fd6fa1443de46c90006da62a610687e7
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-04 14:11:43 +0200

    IsCube

---
+++ Codewars/IsCubes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class IsCubes
+    {
+        public bool IsCube(double volume, double side)
+        {
+            double expectedVolume = 0;
+            if (volume <= 0 || side <= 0)
+                return false;
+            else
+            {
+                 expectedVolume = side * side * side;
+            }
+            return volume == expectedVolume;
+        }
+    }
+}
