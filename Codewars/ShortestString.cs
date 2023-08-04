b4ada182cc2b3470be591dfd53af2f2511abc9fd
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-04 14:04:52 +0200

    FindShort

---
+++ Codewars/ShortestString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ShortestString
+    {
+        public static int FindShort(string s)
+        {
+            string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            int shortestLength = words[0].Length;
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                int wordLength = words[i].Length;
+                if (wordLength < shortestLength)
+                {
+                    shortestLength = wordLength;
+                }
+            }
+
+            return shortestLength;
+        }
+    }
+}
