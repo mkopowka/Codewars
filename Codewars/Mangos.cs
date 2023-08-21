31feff4b04a8fa9dca18981d51e2e066470ba98e
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-21 10:50:12 +0200

    Mango

---
+++ Codewars/Mangos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Mangos
+    {
+        public static int Mango(int quantity, int price)
+        {
+            int setOfTrhee = quantity / 3;
+            int remainingMangoes =quantity % 3;
+
+            int totalCost = (setOfTrhee * 2 + remainingMangoes) * price;
+
+            return totalCost;
+        }
+    }
+}
