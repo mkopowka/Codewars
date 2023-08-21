9bb9246d86254b7052bd85d78094f734eea48781
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-21 09:05:24 +0200

    LowercaseCountChecks

---
+++ Codewars/LowercaseCountChecks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class LowercaseCountChecks
+    {
+        public static int LowercaseCountCheck(string s)
+        {
+            int countLower = 0;
+            foreach (char c in s)
+            {
+                if(char.IsLower(c))
+                {
+                    countLower++;
+                }
+            }
+            return countLower;
+        }
+    }
+}
