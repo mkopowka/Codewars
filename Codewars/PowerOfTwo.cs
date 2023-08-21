366ab08cb5ddd3ab930d47ff935c0f457e313eea
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-21 09:01:27 +0200

    PowerOfTwo

---
+++ Codewars/PowerOfTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class PowerOfTwo
+    {
+        public static bool PowerOfTwos(int n)
+        {
+            return n > 0 && (n & (n - 1)) == 0;
+
+        }
+    }
+}
