0cbab31f0914df2d7f6047eb23f8042182db8541
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-21 09:19:04 +0200

    Greetings

---
+++ Codewars/Greetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Greetings
+    {
+        public static string Greet(string name)
+        {
+            string lowercaseRest = name.Substring(1).ToLower();
+            char firstChar = char.ToUpper(name[0]);
+
+            string names = firstChar + lowercaseRest;
+            return "Hello " + names+"!";
+        }
+    }
+}
