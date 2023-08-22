acc7ad93df8b9105aba5d73583bd3603edb5cfb8
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-22 22:51:08 +0200

    GetRealFloor v2

--- Codewars/RealFloor.cs
+++ Codewars/RealFloor.cs
@@ -10,25 +10,9 @@ namespace Codewars
     {
         public static int GetRealFloor(int n)
         {
-            if (n >= 0)
-            {
-                if (n == 0)
-                {
-                    return 0;
-                }
-                else if (n < 13)
-                {
-                    return n - 1;
-                }
-                else
-                {
-                    return n - 2;
-                }
-            }
-            else
-            {
-                return n;
-            }
+            if (n > 12) return n - 2;
+            if (n > 0) return n - 1;
+            return n;
         }
     }
 }
