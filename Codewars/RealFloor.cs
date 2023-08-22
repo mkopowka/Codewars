b17cdf2e2676604a7fa230eeb84dcb7c58908a8f
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-22 22:50:45 +0200

    RealFloor

---
+++ Codewars/RealFloor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class RealFloor
+    {
+        public static int GetRealFloor(int n)
+        {
+            if (n >= 0)
+            {
+                if (n == 0)
+                {
+                    return 0;
+                }
+                else if (n < 13)
+                {
+                    return n - 1;
+                }
+                else
+                {
+                    return n - 2;
+                }
+            }
+            else
+            {
+                return n;
+            }
+        }
+    }
+}
