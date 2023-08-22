4cbcfa8fcd96fb39bf1749c06dd49a404ee2b3db
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-22 10:21:02 +0200

    Applez

---
+++ Codewars/Apple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Apple
+    {
+        public static string Applez(object n)
+        {
+            if (int.TryParse(n.ToString(), out int intValue))
+            {
+                if (intValue * intValue > 1000) return "It's hotter than the sun!!";
+            }
+            return "Help yourself to a honeycomb Yorkie for the glovebox.";
+        }
+    }
+}
