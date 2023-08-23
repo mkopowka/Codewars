538fc7f08b1b7b1b11a6de8a6642238b147d4d30
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-23 08:34:45 +0200

    Bumps

---
+++ Codewars/Bumps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Bumps
+    {
+        public static string Bump(string input)
+        {
+            int count = 0;
+            foreach (char c in input) { 
+            
+                if(c=='n')
+                {
+                    count++;
+                }
+            }
+            if (count <= 15)
+            {
+                return "Woohoo!";
+            }
+            else return "Car Dead";
+        }
+    }
+}
