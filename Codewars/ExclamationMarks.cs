ef9168d4cb321b039bf454533b676b577e975e99
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-24 10:41:06 +0200

    SumCubes

---
+++ Codewars/ExclamationMarks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ExclamationMarks
+    {
+        public static string Remove(string s)
+        { 
+            return "";
+        }
+    }
+}
---
+++ Codewars/SumOfCubes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumOfCubes
+    {
+        public long SumCubes(int n)
+        {
+            long sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                long cubedValue = (long)i * i * i;
+                sum += cubedValue;
+            }
+            return sum;
+        }
+    }
+}
