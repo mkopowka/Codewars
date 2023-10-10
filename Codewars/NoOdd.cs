51da8386f7cdb872579da33236bb327e8ac04a28
Mateusz Kopówka <mkopowka@hotmail.com>
2023-10-10 17:46:57 +0200

    NoOdds

---
+++ Codewars/NoOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class NoOdd
+    {
+        public static int[] NoOdds(int[] values)
+        {
+            List<int> result = new List<int>(); 
+           foreach (int value in values)
+            {
+                if(value % 2 ==0)
+                {
+                    result.Add(value);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
