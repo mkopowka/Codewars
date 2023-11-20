4f1a91acefba8cb504e6b3d2771299af9282ec02
Mateusz Kopówka <mkopowka@hotmail.com>
2023-11-20 11:22:24 +0100

    odds

---
+++ Codewars/Odd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Odd
+    {
+        public static List<int> Odds(List<int> values) =>
+      // arrow it
+      values.Where( a => a%2!=0 )
+            .ToList();
+    }
+}
