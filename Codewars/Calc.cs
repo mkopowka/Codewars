af2b02f9ad3f04bc5486009a0297bd3b3d9f7991
Mateusz Kopówka <mkopowka@hotmail.com>
2023-11-21 11:28:20 +0100

    CalcCalc

---
+++ Codewars/Calc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Calc
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Mod(int a, int b)
+        {
+            return a % b;
+        }
+        public static int Exponent(int a, int b)
+        {
+            return (int)Math.Pow(a, b);
+        }
+        public static int Subt(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
