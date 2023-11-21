1a9703ff8991bdc5bb13b366f75b487d9b047108
Mateusz Kopówka <mkopowka@hotmail.com>
2023-11-21 10:10:19 +0100

    Switchero

---
+++ Codewars/Switchero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Switchero
+    {
+        public static string Switcheroo(string x)
+        {
+
+            char[] input = x.ToCharArray();
+
+            for (int i = 0;i<x.Length;i++)
+            {
+                if (input[i] =='a')
+                {
+                    input[i] = 'b';
+                }else if (input[i] == 'b')
+                {
+                    input[i] = 'a';
+                }
+            }
+            return new string(input.ToArray());
+        }
+    }
+}
