17a3fbbd51b59ba0f39b73b811e7dc0c044cf9a6
Mateusz Kopówka <mkopowka@hotmail.com>
2023-11-22 19:48:34 +0100

    ToBinarys

--- Codewars/ToBinary.cs
+++ Codewars/ToBinary.cs
@@ -10,20 +10,7 @@ namespace Codewars
     {
         public static int ToBinarys(int n)
         {
-            if(n == 0) return 0;
-            int binary = 0;
-            int baseValue = 1;
-            while(n > 0)
-            {
-                int rem = n % 2;
-
-                binary += rem * baseValue;
-                baseValue *= 10;
-
-                n /= 2;
-            }
-
-            return binary;
+            return Convert.ToInt32(Convert.ToString(n, 2));
         }
     }
 }
