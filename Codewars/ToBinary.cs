b58bdd3963926546f3c6cf9791b3bb6ad50c2152
Mateusz Kopówka <mkopowka@hotmail.com>
2023-11-22 19:48:15 +0100

    ToBinary

---
+++ Codewars/ToBinary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ToBinary
+    {
+        public static int ToBinarys(int n)
+        {
+            if(n == 0) return 0;
+            int binary = 0;
+            int baseValue = 1;
+            while(n > 0)
+            {
+                int rem = n % 2;
+
+                binary += rem * baseValue;
+                baseValue *= 10;
+
+                n /= 2;
+            }
+
+            return binary;
+        }
+    }
+}
