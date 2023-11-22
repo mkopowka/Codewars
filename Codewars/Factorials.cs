4b9f7984da2be8b25e46b18ff0cda7100b10eefd
Mateusz Kopówka <mkopowka@hotmail.com>
2023-11-22 19:12:00 +0100

    Factorial

---
+++ Codewars/Factorials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Factorials
+    {
+        public static ulong Factorial(int n)
+        {
+            if(n == 0 || n==1) return 1;
+            ulong result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= (ulong)i;
+            }
+
+            return result;
+        }
+    }
+}
