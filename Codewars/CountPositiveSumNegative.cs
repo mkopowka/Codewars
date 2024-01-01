9e24a9a7540ad5b0ba4ae1796777464f6a202d43
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-01 10:44:44 +0100

    CountPositiveSumNegative

---
+++ Codewars/CountPositiveSumNegative.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class CountPositiveSumNegative
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            if (input == null || input.Length == 0)
+            {
+
+                return new int[0];
+            }
+
+            int countPositives = 0;
+            int sumNegatives = 0;
+
+            foreach (int num in input)
+            {
+                if (num > 0)
+                {
+                    countPositives++;
+                }
+                else if (num < 0)
+                {
+                    sumNegatives += num;
+                }
+            }
+
+
+            return new int[] { countPositives, sumNegatives };
+        }
+    }
+}
