7d226beae2d51a74cc63ea8f2e3c1bbecdaff87c
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-01 10:17:05 +0100

    AlphabetPosition

---
+++ Codewars/AlphabetPosition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class AlphabetPosition
+    {
+        public static string Alphabetposition(string text)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    int position = char.ToUpper(c) - 'A' + 1;
+                    result.Append(position + " ");
+                }
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
