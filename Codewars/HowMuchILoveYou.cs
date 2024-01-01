164ec340ec0b4ab8f2483ea664c029b26bd9dd7a
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-01 17:12:22 +0100

    HowMuchILoveYou

---
+++ Codewars/HowMuchILoveYou.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class HowMuchILoveYou
+    {
+        public static string HowMuchILoveYous(int nb_petals)
+        {
+            string[] phrases = { "I love you", "a little", "a lot", "passionately", "madly", "not at all" };
+            int index = (nb_petals - 1) % phrases.Length;
+            return phrases[index];
+        }
+    }
+}
