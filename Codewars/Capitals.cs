f82f4c3c036ebf4dbe8046eb8425c498e012e74c
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-01 10:36:22 +0100

    Capitals

---
+++ Codewars/Capitals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Capitals
+    {
+        public static int[] Capitalz(string word)
+        {
+            return Regex.Matches(word, "[A-Z]").Select(m => m.Index).ToArray();
+
+        }
+    }
+}
