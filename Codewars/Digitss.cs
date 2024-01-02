8242ac2ad5e3efd0944bc00d9a2b9c569828cda6
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-02 11:53:36 +0100

    Digits

---
+++ Codewars/Digitss.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Digitss
+    {
+        public static int Digits(ulong n)
+        {
+            string value = n.ToString();
+            int counter = 0;
+            foreach (char c in value)
+            {
+                counter++;
+            }
+            return counter;
+        }
+    }
+}
