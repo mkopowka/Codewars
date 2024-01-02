b0d7b1210a56e0e2b5aef81f4f60f682f378b662
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-02 12:08:04 +0100

    ShowSequence

---
+++ Codewars/ShowSequence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ShowSequence
+    {
+        public static string ShowSequences(int n)
+        {
+            if (n < 0)
+            {
+                return $"{n}<0";
+            }
+
+            if (n == 0)
+            {
+                return "0=0";
+            }
+
+            int sum = 0;
+            string sequence = "0";
+
+            for (int i = 1; i <= n; i++)
+            {
+                sequence += "+" + i;
+                sum += i;
+            }
+
+            return $"{sequence} = {sum}";
+        }
+    }
+}
