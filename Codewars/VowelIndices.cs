9b8c2deda45521726077f7d1cb34e7c0c91e57b4
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-02 12:01:11 +0100

    VowelIndicesFindVowelIndices

---
+++ Codewars/VowelIndices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class VowelIndices
+    {
+        static int[] FindVowelIndices(string word)
+        {
+            int vowelCount = 0;
+
+            foreach (char letter in word)
+            {
+                if (IsVowel(letter))
+                {
+                    vowelCount++;
+                }
+            }
+
+            int[] vowelIndices = new int[vowelCount];
+            int currentIndex = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentLetter = Char.ToLower(word[i]);
+
+                if (IsVowel(currentLetter))
+                {
+                    vowelIndices[currentIndex++] = i + 1;
+                }
+            }
+
+            return vowelIndices;
+        }
+        static bool IsVowel(char character)
+        {
+            return "aeiouyAEIOUY".Contains(Char.ToLower(character));
+        }
+    }
+
+}
