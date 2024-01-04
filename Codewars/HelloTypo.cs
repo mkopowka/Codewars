78c12c122288b0fce624837428be02ec0d4af706
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-04 13:12:36 +0100

    HelloTypo

---
+++ Codewars/HelloTypo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class HelloTypo
+    {
+        /*
+         * Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+            Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+        */
+        public static string Hello(string name="")
+        {
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return "Hello, World!";
+            }
+            string formattedName = char.ToUpper(name[0]) + name.Substring(1).ToLower();
+
+            return $"Hello, {formattedName}!";
+        }
+    }
+}
