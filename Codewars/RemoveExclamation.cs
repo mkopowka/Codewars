49a22d20a44bcf90c6f0d6241396d8219000b431
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-05 17:25:26 +0100

    RemoveExclamation

---
+++ Codewars/RemoveExclamation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. 
+     * For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+      */
+    internal class RemoveExclamation
+    {
+        public static string Remove(string s)
+        {
+            string withoutExclamation = s.Replace("!", "");
+            string result = withoutExclamation + "!";
+            return result;
+
+        }
+    }
+}
