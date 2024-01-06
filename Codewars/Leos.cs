08264a3806efdd405dd5d60f3baa818ff54ae241
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-06 21:06:58 +0100

    Leo

---
+++ Codewars/Leos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * You have to write a function that describe Leo:
+
+        def leo(oscar):
+          pass
+        if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+        if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+        if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+        if it was over 88 you should return "Leo got one already!"
+    */
+    internal class Leos
+    {
+
+        public static string Leo(int oscar)
+        {
+            if (oscar == 88) return "Leo finally won the oscar! Leo is happy";
+            if (oscar == 86 ) return "Not even for Wolf of wallstreet?!";
+            
+            if (oscar > 88) return "Leo got one already!"; else
+                return "When will you give Leo an Oscar?";
+        }
+
+    }
+}
