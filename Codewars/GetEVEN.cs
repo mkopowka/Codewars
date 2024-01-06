b4ee68cd292f2a8bcddaa04fb7932c9466c4bc27
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-06 23:22:24 +0100

    GetEven

---
+++ Codewars/GetEVEN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Starting with .NET Framework 3.5, C# supports a Where (in the System.Linq namespace) method which allows a user to filter arrays based on a predicate. Use the Where method to complete the function given.
+
+        Enumerable.Where documentation: https://msdn.microsoft.com/en-us/library/bb534803(v=vs.110).aspx
+
+        The solution would work like the following:
+    */
+    internal class GetEven
+    {
+        public static int[] GetEvenNumbers(int[] numbers)
+        {
+            return numbers.Where(numbers => numbers %2 == 0).ToArray();
+        }
+    }
+}
