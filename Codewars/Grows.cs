17576ba4f9c2b070c0ccddc2fe41eede2b1dd519
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-06 23:37:34 +0100

    Grow

---
+++ Codewars/Grows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Given a non-empty array of integers, return the result of multiplying the values together in order. */
+    internal class Grows
+    {
+        public static int Grow(int[] x)
+        {
+            int result = 1;
+            foreach (int num in x)
+            {
+                result *= num;
+            }
+            return result;
+        }
+    }
+
+}
