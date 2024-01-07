44a2d933dc2cf25df0a86e4353f2d46eb95ea2ea
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-07 16:30:50 +0100

    FindLongest

---
+++ Codewars/FindLongests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Find the number with the most digits.
+
+        If two numbers in the argument array have the same number of digits, return the first one in the array.
+    */
+    internal class FindLongests
+    {
+        public static int FindLongest(int[] number)
+        {
+            return number.OrderByDescending(n => n.ToString().Length).First();
+        }
+    }
+}
