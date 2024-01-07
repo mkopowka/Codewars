89f88b378f3cb517be2e798309aecfaa03b76466
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-07 11:36:43 +0100

    SquareAreas

---
+++ Codewars/SquareAreas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+     */
+    internal class SquareAreas
+    {
+        public static double SquareArea(double A)
+        {
+            double radius = A / (2 * Math.PI / 4);
+            double area = (Math.Pow(radius, 2));
+            return Math.Round(area, 2);
+        }
+    }
+}
