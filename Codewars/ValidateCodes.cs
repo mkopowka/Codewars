20e6b774cac8be079724e4107cbfe7ea477a4315
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-07 12:23:24 +0100

    ValidateCode

---
+++ Codewars/ValidateCodes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+     * */
+    internal class ValidateCodes
+    {
+        public static bool ValidateCode(string code)
+        {
+            if (code.StartsWith("1") || code.StartsWith("2") || code.StartsWith("3")) return true;
+            else return false;
+        }
+    }
+}
