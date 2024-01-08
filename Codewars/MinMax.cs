830ec0d34eafe769cb2d181a70f55a88062ccc8a
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-08 11:39:16 +0100

    MinMax

--- Codewars/MinMax.cs
+++ Codewars/MinMax.cs
@@ -11,28 +11,7 @@ namespace Codewars
         /* Write a function that returns both the minimum and maximum number of the given list/array. */
         public static int[] minMaxs(int[] lst)
         {
-            if (lst == null || lst.Length == 0)
-            {
-                return null;
-            }
-
-            int min = lst[0];
-            int max = lst[0];
-
-            for (int i = 1; i < lst.Length; i++)
-            {
-                if (lst[i] < min)
-                {
-                    min = lst[i];
-                }
-
-                if (lst[i] > max)
-                {
-                    max = lst[i];
-                }
-            }
-
-            return new int[] { min, max };
+            return new int[] { lst.Min(), lst.Max() };
         }
     }
 }
