6c72f3f4168ba36ac247ae06b759652c737d6d88
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-09 16:08:32 +0100

    OrderCount

---
+++ Codewars/OrderCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+     */
+
+    internal class OrderCount
+    {
+        public static List<Tuple<char, int>> OrderedCount(string input)
+        {
+            return input.GroupBy(x=>x).Select(x=>Tuple.Create(x.Key,x.Count())).ToList();
+        }
+    }
+}
