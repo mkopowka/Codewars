1ac83229bd7cfa1564af135b967a1d5f99ba7fd4
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-09 21:09:47 +0100

    GetASCIIs

---
+++ Codewars/GetASCII.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Get ASCII value of a character. */
+    internal class GetASCII
+    {
+        public static int GetASCIIs(char c)
+        {
+            return Convert.ToInt32(c);
+        }
+    }
+}
