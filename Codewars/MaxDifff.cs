4468a16e2935e88c51635a4e64efcc6eccf480ff
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-09 21:01:23 +0100

    MaxDiff

---
+++ Codewars/MaxDifff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter. */
+    internal class MaxDifff
+    {
+        public int MaxDiff(int[] lst)
+        {
+           int min = lst.Min();
+           int max = lst.Max();
+
+            if (lst.Length == 0)
+            {
+                return 0;
+            }
+            return max - min;
+        }
+    }
+}
