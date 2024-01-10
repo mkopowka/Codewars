a69543a86b0ea5a9baed1ba02822cb696c7b0dc1
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-10 17:05:07 +0100

    Websitez

--- Codewars/Websitez.cs
+++ Codewars/Websitez.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Codewars
 {
+    /* This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+    Add the value "codewars" to the array websites 1,000 times.
+    */
     public class Websitez
     {
         public static string[] Websites = Enumerable.Repeat("codewars",1000).ToArray();
