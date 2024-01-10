e88964ed6fba1cea7eea5a1f43e688216ceba157
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-10 16:14:33 +0100

    SortMyStrings

---
+++ Codewars/SortMyStrings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SortMyStrings
+    {
+        /* Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below) */
+        public string SortMyString(string s)
+        {
+
+            string odd ="";
+            string even = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    even += s[i];
+                }
+                else
+                {
+                    odd += s[i];
+                }
+            }
+            return even + " " + odd ;
+        }
+    }
+}
