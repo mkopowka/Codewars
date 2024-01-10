25a91f0534dbd2076ce99fea99de967db8886bdb
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-10 17:00:18 +0100

    Websitez

---
+++ Codewars/Websitez.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public class Websitez
+    {
+        public static string[] Websites = Enumerable.Repeat("codewars",1000).ToArray();
+
+
+    }
+}
