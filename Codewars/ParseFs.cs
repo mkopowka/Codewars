bfa844a40a2354da526d9768e867cc8925f8e116
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-11 10:56:59 +0100

    ParseFs

---
+++ Codewars/ParseFs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class ParseFs
+        /* Write function parseFloat which takes an input and returns a number or Nothing if conversion is not possible. */
+    {
+        public static double? ParseF(object s = null)
+        {
+            if (s != null && double.TryParse(s.ToString(), out double result))
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
