403e188107461ad0d521789f6eab2bd6fc3fc1fa
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-11 10:37:22 +0100

    getNumberFromStringZ

---
+++ Codewars/SumDigPows.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class SumDigPows
+    {
+        public static long[] SumDigPow(long a, long b)
+        {
+
+            //TODO
+            //TODO
+        }
+    }
+}
---
+++ Codewars/getNumberFromStringZ.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56 */
+    internal class getNumberFromStringZ
+    {
+        public static int getNumberFromString(string s)
+        {
+            return int.Parse(string.Concat(s.Where(char.IsDigit)));
+        }
+    }
+}
