50295d62faf84ff9b033d24cec13d7bce064fea7
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-12 17:49:42 +0100

    DuckDuckGooses

---
+++ Codewars/DuckDuckGooses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen. */
+    internal class DuckDuckGooses
+    {
+        public static string DuckDuckGoose(Player[] players, int goose)
+        {
+            int adjustedIndex = (goose - 1) % players.Length;
+            return players[adjustedIndex].Name;
+        }
+    }
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
