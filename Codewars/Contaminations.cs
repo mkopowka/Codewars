66fa4a16b8e57383f3d991ba4c9d73cfcc1b0414
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-13 16:29:42 +0100

    Contamination

---
+++ Codewars/Contaminations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+         * An AI has infected a text with a character!!
+
+    This text is now fully mutated to this character.
+
+    If the text or the character are empty, return an empty string.
+    There will never be a case when both are empty as nothing is going on!!
+
+    Note: The character is a string of length 1 or an empty string.
+    */
+    internal class Contaminations
+    {
+        public static string Contamination(string text, string character)
+        {
+
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(character))
+            {
+                return "";
+            }
+            string mutated = new string(character[0], text.Length);
+            return mutated;
+        }
+    }
+}
