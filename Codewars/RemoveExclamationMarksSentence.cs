444db6953da2989cf127aff64afe5d13a76fa31f
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-13 15:56:08 +0100

    Remove

---
+++ Codewars/RemoveExclamationMarksSentence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Remove all exclamation marks from the end of sentence.
+
+        */
+    internal class RemoveExclamationMarksSentence
+    {
+        public static string Remove(string s)
+        {
+            while (s.EndsWith('!'))
+            {
+                s = s.Substring(0, s.Length - 1);
+            }
+            return s;
+        }
+    }
+}
