6a0e3f14a202e271da65014f88011a7c9617a7b3
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-15 20:52:41 +0100

    MultipleOfIndex

---
+++ Codewars/MultipleOfIndexes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Return a new array consisting of elements which are multiple of their own index in input array (length > 1). */
+    //todo
+    internal class MultipleOfIndexes
+    {
+        public static List<int> MultipleOfIndex(List<int> xs)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 1; i < xs.Count; i++)
+            {
+                if (xs[i] % i == 0)
+                {
+                    result.Add(xs[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
