4d6173152734f04095b2e0af8ec3b2a0e6e5a51e
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-15 09:06:54 +0100

    Lasts

---
+++ Codewars/FuelPrices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class FuelPrices
+    {
+        public static double FuelPrice(double litres, double pricePerLitre)
+        {
+            //TODO
+            return litres;
+        }
+    }
+}
---
+++ Codewars/Lasts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+        If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+    */
+    internal class Lasts
+    {
+        public static string[] Last(string x)
+        {
+            string[] words = x.Split(' ');
+            string[] sortedWords = words.OrderBy(word => word.Last()).ToArray();
+
+            return sortedWords;
+        }
+    }
+}
