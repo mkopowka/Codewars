d7c5a53c77c564064eff38db37a9712446168d3d
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-16 08:49:50 +0100

    PlayTurns

---
+++ Codewars/PlayTurns.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status. */
+    internal class PlayTurns
+    {
+        public static int Health = 100;
+        public static int Position = 0;
+        public static int Coins = 0;
+
+        public static void PlayTurn()
+        {
+            RollDice();
+            Move();
+            Combat();
+            GetCoins();
+            BuyHealth();
+            PrintStatus();
+        }
+        static void main()
+        {
+        }
+
+ 
+
+    }
+}
