7fe3283740d0e0d461ac1b86776c51bc7efbc1aa
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-20 09:38:38 +0100

    MinValue

---
+++ Codewars/MinValues.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). */
+    internal class MinValues
+    {
+        public static long MinValue(int[] a)
+        {
+            int[] uniqueDigits = a.Distinct().OrderBy(d => d).ToArray();
+            string minValueStr = string.Join("", uniqueDigits);
+            int minValue = int.Parse(minValueStr);
+
+            return minValue;
+        }
+    }
+}
