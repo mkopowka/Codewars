623055b98777d5bf7128ea8aa460d1dbc3e61329
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-20 09:40:33 +0100

    Ships

---
+++ Codewars/Ships.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+
+Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+    */
+    public class Ships
+    {
+        public int Draft;
+        public int Crew;
+
+        public Ships(int draft, int crew)
+        {
+            Draft = draft;
+            Crew = crew;
+        }
+
+        public bool IsWorthIt()
+        {
+            double effectiveDraft = Draft - (Crew * 1.5);
+            return effectiveDraft > 20;
+        }
+    }
+}
