7865597d4fc35740da52024df2f7191e44d5ea74
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-21 08:51:40 +0100

    CombineNames

---
+++ Codewars/CombineNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Create a function named (Combine_names) that accepts two parameters (first and last name). The function should return the full name. */
+    internal class CombineNames
+    {
+
+        public string CombineName(string a, string b)
+        {
+            return a + " " + b;
+        }
+    }
+}
