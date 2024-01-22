3a39ba235a00252de5902ff18a1479cb50f1314b
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-22 21:25:13 +0100

    Billboard

--- Codewars/Billboards.cs
+++ Codewars/Billboards.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace Codewars
 {
+    /* You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
+
+        You can not use multiplier "*" operator.
+    */
     internal class Billboards
     {
         public static double Billboard(string name, double price = 30)
         {
-            //TODO
-            return 0.0;
+            double nameCost = 0;
+
+            foreach (char character in name)
+            {
+                nameCost += price;
+            }
+
+            return nameCost;
         }
     }
 }
