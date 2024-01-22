38668201eee25a37889d57ef74d0d55cca4344d6
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-22 21:26:42 +0100

    CountRedBeads

---
+++ Codewars/CountRedBead.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+     */
+    internal class CountRedBead
+    {
+        public static int CountRedBeads(int n)
+        {
+            if (n < 2)
+            {
+                return 0;
+            }
+            return (n - 1) * 2;
+        }
+    }
+}
