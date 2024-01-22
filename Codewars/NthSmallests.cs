b5af10ccfe90789f8ede1a366a423537234837d6
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-22 21:29:24 +0100

    NthSmallest

---
+++ Codewars/NthSmallests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Given an array/list of integers, find the Nth smallest element in the array.
+     */
+    internal class NthSmallests
+    {
+        public static int NthSmallest(int[] arr, int pos)
+        {
+            Array.Sort(arr);
+            return arr[pos - 1];
+        }
+    }
+}
