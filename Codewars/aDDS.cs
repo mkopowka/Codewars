d4eb07488e3b99b15f337a6cea19074ccc48ba3b
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-22 21:31:12 +0100

    Adds

---
+++ Codewars/aDDS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Create a function add(n)/Add(n) which returns a function that always adds n to any number &*/
+    internal class Adds
+    {
+        public static Func<double, double> Add(double n)
+        {
+            return x => x + n;
+        }
+    }
+}
