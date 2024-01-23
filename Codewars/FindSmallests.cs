f497636264a820a0f8ebdab1a67721d0b50c73b8
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-23 09:40:41 +0100

    FindSmallest

---
+++ Codewars/FindSmallests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+    Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+    */
+    internal class FindSmallests
+    {
+        public static int FindSmallest(int[] numbers, string toReturn)
+        {
+            int minValue = numbers[0];
+            int minIndex = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < minValue)
+                {
+                    minValue = numbers[i];
+                    minIndex = i;
+                }
+            }
+
+            return toReturn == "value" ? minValue : minIndex;
+        }
+    }
+}
