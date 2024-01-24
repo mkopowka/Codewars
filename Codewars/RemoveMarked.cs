2240a80f4b0def1ba04c2a90fffac0942b966f0b
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-24 10:01:10 +0100

    RemoveMarked

---
+++ Codewars/RemoveMarked.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Define a method/function that removes from a given array of integers all the values contained in a second array.
+
+ */
+    internal class RemoveMarked
+    {
+        public static int[] Remove(int[] integerList, int[] valuesList)
+        {
+            return integerList.Where(x => !valuesList.Contains(x)).ToArray();
+        }
+    }
+}
