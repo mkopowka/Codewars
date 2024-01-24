3d4fc1b8a0bd97f2bb1aebe5a68d3993814cf169
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-24 10:08:54 +0100

    LogicalCalcs

---
+++ Codewars/LogicalCalcs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+
+*/
+    internal class LogicalCalcs
+    {
+        public static bool LogicalCalc(bool[] array, string op)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return false;
+            }
+
+            bool result = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (op == "AND")
+                {
+                    result = result && array[i];
+                }
+                else if (op == "OR")
+                {
+                    result = result || array[i];
+                }
+                else if (op == "XOR")
+                {
+                    result = result != array[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
