379c11a3e50cfb00e8a41681a4f2c2e8118fdf41
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-25 11:53:44 +0100

    IsDigits

---
+++ Codewars/IsDigits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not. */
+    internal class IsDigits
+    {
+        public static bool IsDigit(string s)
+        {
+            return double.TryParse(s, out _);
+        }
+    }
+}
