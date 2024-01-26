89d6e9958744ceb6da5f71f4e44fa0d91347bb27
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-26 11:11:12 +0100

    DefineSuit

---
+++ Codewars/Cards.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{/* You get any card as an argument. Your task is to return the suit of this card (in lowercase). */
+    internal class Cards
+    {
+        public static string DefineSuit(string card)
+        {
+            char suitSymbol = card[card.Length - 1];
+
+            switch (suitSymbol)
+            {
+                case '♣':
+                    return "clubs";
+                case '♦':
+                    return "diamonds";
+                case '♥':
+                    return "hearts";
+                case '♠':
+                    return "spades";
+                default:
+                    throw new ArgumentException("Invalid card: " + card);
+            }
+        }
+    }
+}
