c385073ec72793b58a7e21e8dcc857a2ab2693de
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-27 10:35:28 +0100

    Largest

---
+++ Codewars/Largests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class Largests
+    {
+        public static List<int> Largest(int n, List<int> xs)
+        {
+            if (n <= 0 || xs == null || xs.Count == 0)
+            {
+                return new List<int>();
+            }
+            return xs.OrderByDescending(x => x).Take(n).ToList();
+        }
+    }
+}
