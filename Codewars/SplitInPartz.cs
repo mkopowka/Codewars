e1cb670a890ea7efb7102ed4f69644931df5e0c0
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-29 11:54:09 +0100

    SplitInParts

---
+++ Codewars/SplitInPartz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method) */
+    internal class SplitInPartz
+    {
+        public static string SplitInParts(string s, int partLength)
+        {
+
+            for (int i = partLength; i < s.Length; i += partLength + 1)
+                s = s.Insert(i, " ");
+            return s;
+        }
+    }
+}
