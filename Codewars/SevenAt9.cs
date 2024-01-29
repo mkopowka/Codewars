60f457f1c3094c83f06a48ba8b6e038a335fbd7b
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-29 11:49:14 +0100

    SevenAteNine

---
+++ Codewars/SevenAt9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Write a function that removes every lone 9 that is inbetween 7s. */
+
+
+    internal class SevenAt9
+    {
+        public static string SevenAteNine(string str)
+        {
+            char[] chars = str.ToCharArray();
+
+            for (int i = 1; i < chars.Length - 1; i++)
+            {
+                if (chars[i - 1] == '7' && chars[i] == '9' && chars[i + 1] == '7')
+                {
+                    chars[i] = '7';
+                }
+            }
+
+            return new string(chars).Replace("97", "");
+        }
+    }
+}
