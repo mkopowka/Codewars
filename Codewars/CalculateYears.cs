385c6380f2daf401307000d3336bf5bd51c14a20
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-29 11:50:46 +0100

    CalculateYears

---
+++ Codewars/CalculateYears.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class CalculateYears
+    {
+        public static int CalculateYearz(double principal, double interest, double tax, double desiredPrincipal)
+        {
+            int years = 0;
+
+            while (principal < desiredPrincipal)
+            {
+                double interestEarned = principal * interest;
+                double taxPaid = interestEarned * tax;
+                principal += interestEarned - taxPaid;
+                years++;
+            }
+
+            return years;
+        }
+    }
+}
