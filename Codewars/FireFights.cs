6ec8b70e7696677c5baf57dd349192a00ff4a595
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-30 10:48:42 +0100

    FireFights

---
+++ Codewars/FireFights.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Enjoying your holiday, you head out on a scuba diving trip!
+
+        Disaster!! The boat has caught fire!!
+
+        You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+        Go to work!
+    */
+    internal class FireFights
+    {
+        public static string FireFight(string s)
+        {
+            string result = s.Replace("Fire", "~~");
+
+            return result;
+        }
+    }
+}
