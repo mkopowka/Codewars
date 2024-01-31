648a1b37f36e63a56f79562c998fb99037ea457c
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-31 10:58:09 +0100

    lostSheeps

---
+++ Codewars/lostSheeps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * Every Friday and Saturday night, farmer counts amount of sheep returned back to his farm (sheep returned on Friday stay and don't leave for a weekend).
+
+    Sheep return in groups each of the days -> you will be given two arrays with these numbers (one for Friday and one for Saturday night). Entries are always positive ints, higher than zero.
+
+    Farmer knows the total amount of sheep, this is a third parameter. You need to return the amount of sheep lost (not returned to the farm) after final sheep counting on Saturday.
+    */
+    internal class lostSheeps
+    {
+        public int lostSheep(int[] friday, int[] saturday, int total)
+        {
+            return total - friday.Sum() - saturday.Sum();
+        }
+    }
+}
