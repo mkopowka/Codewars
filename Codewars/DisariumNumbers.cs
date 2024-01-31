fbb239f12ac0c9b4979c9214fb1477053ff0e89c
Mateusz Kopówka <mkopowka@hotmail.com>
2024-01-31 11:43:11 +0100

    DisariumNumbers

---
+++ Codewars/DisariumNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    internal class DisariumNumbers
+    {
+        public static string DisariumNumber(int number)
+        {
+            int temp = number;
+            int length = number.ToString().Length;
+            int sum = 0;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += (int)Math.Pow(digit, length);
+                temp /= 10;
+                length--;
+            }
+
+            return sum == number ? "Disarium !!" : "Not !!";
+        }
+    }
+}
