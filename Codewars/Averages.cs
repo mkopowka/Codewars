b92ff7a146136e257c8d96c99bf202ba07a3ecfd
Mateusz Kopówka <mkopowka@hotmail.com>
2024-02-02 11:18:57 +0100

    Average

---
+++ Codewars/Averages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+
+The array will never be empty.
+    */
+    internal class Averages
+    {
+        public static int Average(int[] scores)
+        {
+            return (int)Math.Round(scores.Average());
+        }
+    }
+}
