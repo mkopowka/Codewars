92f3e6b5836d19724097601dda6b951070d8fe4d
Mateusz Kopówka <mkopowka@hotmail.com>
2024-02-03 16:20:37 +0100

    EachCons

--- Codewars/EachCon.cs
+++ Codewars/EachCon.cs
@@ -6,14 +6,11 @@ using System.Threading.Tasks;
 
 namespace Codewars
 {
+    /*Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so: */
     internal class EachCon
     {
         public static IEnumerable<IEnumerable<int>> EachCons(int[] list, int n)
         {
-            if (n <= 0 || n > list.Length)
-            {
-                throw new ArgumentException("Invalid value of n");
-            }
 
             for (int i = 0; i <= list.Length - n; i++)
             {
