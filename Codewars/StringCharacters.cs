92ae2fd22d4da561f02d40d412a95314b448fa70
Mateusz Kopówka <mkopowka@hotmail.com>
2024-02-03 16:18:02 +0100

    StringCharacters

---
+++ Codewars/StringCharacters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters, as follows.
+
+Solve("*'&ABCDabcde12345") = [4,5,5,3]. 
+--the order is: uppercase letters, lowercase, numbers and special characters.
+    */
+    internal class StringCharacters
+    {
+        public static int[] solve(String s)
+        {
+            int upperCount = 0, lowerCount = 0, digitCount = 0, specialCount = 0;
+
+            foreach (char c in s)
+            {
+                if (Char.IsUpper(c))
+                {
+                    upperCount++;
+                }
+                else if (Char.IsLower(c))
+                {
+                    lowerCount++;
+                }
+                else if (Char.IsDigit(c))
+                {
+                    digitCount++;
+                }
+                else
+                {
+                    specialCount++;
+                }
+            }
+
+            return new int[] { upperCount, lowerCount, digitCount, specialCount };
+        }
+    }
+}
