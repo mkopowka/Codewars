a487dd2e01755bcb4df5e4bf98b5056be0ba3a8e
Mateusz Kopówka <mkopowka@hotmail.com>
2024-02-15 21:22:44 +0100

    ClassicHW

---
+++ Codewars/ClassicHW.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*
+     * You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+Note that for some languages, the function main is the entry point of the program.
+    */
+    internal class ClassicHW
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
