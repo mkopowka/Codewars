222f34d3f732f435f0445ab531dbf4107f357eca
Mateusz Kopówka <mkopowka@hotmail.com>
2024-03-01 15:27:38 +0100

    Sortuj

---
+++ Codewars/Sortuj.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /* Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order. */
+    internal class Sortuj
+    {
+        public static string[] SortMe(string[] names)
+        {
+            Array.Sort(names);
+            return names;
+        }
+    }
+}
